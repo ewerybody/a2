e10d03cf0b0e1582ae87e3eb386392f5dbc1a08e
ewerybody <ewerybody+github@gmail.com>
2015-07-16 11:31:47 +0200

    a2db module can now pop entries
    That's still to be used to cleanup the db entries of user settings but
    here it's embedded in the test already... that should be a proper
    unittest somewhen.. :/

--- ui/test/a2dbtest.py
+++ ui/test/a2dbtest.py
@@ -18,7 +18,7 @@ db = a2dblib.A2db(a2setdir)
 log.info('db members:\n\t%s' % [d for d in dir(db) if not d.startswith('_')])
 testTable = 'TEST table'
 
-log.info('\ntesting creating/deleting tables %s' % ('.'*40))
+log.info('\ntesting creating/deleting tables %s' % ('.' * 40))
 log.debug('testTable: "%s" Does NOT Exist: %s' % (testTable, testTable not in db.tables()))
 db.createTable(testTable)
 log.debug('testTable: "%s" Created: %s' % (testTable, testTable in db.tables()))
@@ -26,7 +26,7 @@ db.dropTable(testTable)
 log.debug('testTable: "%s" Deleted: %s' % (testTable, testTable not in db.tables()))
 
 
-log.info('\ntesting "set" %s' % ('.'*40))
+log.info('\ntesting "set" %s' % ('.' * 40))
 setValue = 't32t234fsaassa'
 db.set('bam', setValue, testTable)
 value = db.get('bam', testTable)
@@ -39,7 +39,7 @@ db.dropTable(testTable)
 db._con.commit()
 log.info('tables: %s' % db.tables())
 
-log.info('\ntesting getting/setting dictionaries with JSON %s' % ('.'*40))
+log.info('\ntesting getting/setting dictionaries with JSON %s' % ('.' * 40))
 d = {'name': [1, 2, 3], 'bla': {'alist': ['&\323@!|//'], 'value': 123.523452}}
 log.info('d: %s' % d)
 #j = json.dumps(d, separators=(',', ':'))
@@ -52,6 +52,13 @@ log.info('dx: %s' % dx)
 log.info('dx == d: %s' % (dx == d))
 dd = db.getd('jsonTest', testTable)
 log.info('d == dd: %s' % (d == dd))
+
+log.info('\ndeleting values ".pop" %s' % ('.' * 40))
+key = 'jsonTest'
+log.info('key exists: %s' % (key in db.keys(testTable)))
+db.pop('jsonTest', testTable)
+log.info('key deleted: %s' % (key not in db.keys(testTable)))
+
 
 db.dropTable(testTable)
 log.debug('testTable: "%s" Deleted: %s' % (testTable, testTable not in db.tables()))
