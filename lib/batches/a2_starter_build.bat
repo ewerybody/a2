1822856e03524185ecebfe36582da80286fd2df9
ewerybody <ewerybody+github@gmail.com>
2016-08-26 10:52:38 +0200

    fixed build scripts in new location, rebuild exes

--- lib/batches/a2_starter_build.bat
+++ lib/batches/a2_starter_build.bat
@@ -1,3 +1,4 @@
 set Ahk2Exe="C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe"
-set script=a2_starter
-%Ahk2Exe% /in %script%.ahk /out %script%.exe /icon ui\res\a2.ico /mpress 1
+set script=..\..\a2_starter
+set icon=..\..\ui\res\a2.ico
+%Ahk2Exe% /in %script%.ahk /out %script%.exe /icon %icon% /mpress 1
