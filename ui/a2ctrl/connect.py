4e9b23e4c4201fd0648d128ac36c259c669a06d9
ewerybody <ewerybody+github@gmail.com>
2016-09-01 01:08:13 +0200

    explicit naming of vars and functions

--- ui/a2ctrl/connect.py
+++ ui/a2ctrl/connect.py
@@ -18,11 +18,11 @@ def cfg_controls(cfg, ui_object, prefix='cfg_'):
     browses all members of the given ui object to connect ones named with prefix
     ('cfg_' by default) with a config dict and fill it with current value.
     """
-    for objname, control in inspect.getmembers(ui_object):
+    for objname, ctrl in inspect.getmembers(ui_object):
         if not objname.startswith(prefix):
             continue
         name = objname[4:]
-        control(control, name, cfg)
+        control(ctrl, name, cfg)
 
 
 def control_list(controls, cfg, change_signal=None):
@@ -33,7 +33,7 @@ def control_list(controls, cfg, change_signal=None):
         control(ctrl, ctrl.objectName(), cfg, change_signal)
 
 
-def control(control, name, cfg, change_signal=None):
+def control(ctrl, name, cfg, change_signal=None):
     """
     Connects a single control to a name in the given cfg dict
     """
@@ -50,76 +50,76 @@ def control(control, name, cfg, change_signal=None):
         if state:
             cfg[name] = value
 
-    if isinstance(control, QtGui.QCheckBox):
+    if isinstance(ctrl, QtGui.QCheckBox):
         # checkBox.clicked doesn't send state, so we put the func to check
         # checkBox.stateChanged does! But sends int: 0, 1, 2 for off, tri, on
-        # solution: control.clicked[bool] sends the state already!
-        control.clicked[bool].connect(partial(_updateCfgData, cfg, name))
+        # solution: ctrl.clicked[bool] sends the state already!
+        ctrl.clicked[bool].connect(partial(_updateCfgData, cfg, name))
         if change_signal is not None:
-            control.clicked[bool].connect(change_signal.emit)
+            ctrl.clicked[bool].connect(change_signal.emit)
         # set ctrl according to config or set config from ctrl
         if name in cfg:
-            control.setChecked(cfg[name])
+            ctrl.setChecked(cfg[name])
         else:
-            cfg[name] = control.isChecked()
+            cfg[name] = ctrl.isChecked()
 
-    elif isinstance(control, QtGui.QLineEdit):
-        control.textChanged.connect(partial(_updateCfgData, cfg, name))
+    elif isinstance(ctrl, QtGui.QLineEdit):
+        ctrl.textChanged.connect(partial(_updateCfgData, cfg, name))
         if change_signal is not None:
-            control.textChanged.connect(change_signal.emit)
+            ctrl.textChanged.connect(change_signal.emit)
         if name in cfg:
-            control.setText(cfg[name])
+            ctrl.setText(cfg[name])
         else:
-            cfg[name] = control.text()
+            cfg[name] = ctrl.text()
 
-    elif isinstance(control, PathField):
-        control.changed.connect(partial(_updateCfgData, cfg, name))
+    elif isinstance(ctrl, PathField):
+        ctrl.changed.connect(partial(_updateCfgData, cfg, name))
         if change_signal is not None:
-            control.changed.connect(change_signal.emit)
+            ctrl.changed.connect(change_signal.emit)
         if name in cfg:
-            control.value = cfg[name]
+            ctrl.value = cfg[name]
         else:
-            cfg[name] = control.value
+            cfg[name] = ctrl.value
 
-    elif isinstance(control, QtGui.QComboBox):
-        control.currentIndexChanged.connect(partial(_updateCfgData, cfg, name))
+    elif isinstance(ctrl, QtGui.QComboBox):
+        ctrl.currentIndexChanged.connect(partial(_updateCfgData, cfg, name))
         if change_signal is not None:
-            control.currentIndexChanged.connect(change_signal.emit)
+            ctrl.currentIndexChanged.connect(change_signal.emit)
         if name in cfg:
-            control.setCurrentIndex(cfg[name])
+            ctrl.setCurrentIndex(cfg[name])
         else:
-            cfg[name] = control.currentIndex()
+            cfg[name] = ctrl.currentIndex()
 
-    elif isinstance(control, QtGui.QListWidget):
+    elif isinstance(ctrl, QtGui.QListWidget):
         # so far only to fill the control
         # since it's not a widget that changes data by default
-        #control.itemChanged.connect(partial(_list_widget_test, name))
+        #ctrl.itemChanged.connect(partial(_list_widget_test, name))
         if name in cfg:
-            control.insertItems(0, cfg[name])
+            ctrl.insertItems(0, cfg[name])
         else:
-            items = [control.item(i).text() for i in range(control.count())]
+            items = [ctrl.item(i).text() for i in range(ctrl.count())]
             cfg[name] = items
 
-    elif isinstance(control, (QtGui.QSpinBox, QtGui.QDoubleSpinBox)):
-        control.valueChanged.connect(partial(_updateCfgData, cfg, name))
+    elif isinstance(ctrl, (QtGui.QSpinBox, QtGui.QDoubleSpinBox)):
+        ctrl.valueChanged.connect(partial(_updateCfgData, cfg, name))
         if change_signal is not None:
-            control.valueChanged.connect(change_signal.emit)
+            ctrl.valueChanged.connect(change_signal.emit)
         if name in cfg:
-            control.setValue(cfg[name])
+            ctrl.setValue(cfg[name])
         else:
-            cfg[name] = control.value()
+            cfg[name] = ctrl.value()
 
-    elif isinstance(control, QtGui.QRadioButton):
+    elif isinstance(ctrl, QtGui.QRadioButton):
         name, value = name.rsplit('_', 1)
-        control.toggled.connect(partial(_radio_update, cfg, name, value))
+        ctrl.toggled.connect(partial(_radio_update, cfg, name, value))
         if change_signal is not None:
-            control.toggled.connect(change_signal.emit)
+            ctrl.toggled.connect(change_signal.emit)
         if name in cfg:
-            control.setChecked(cfg[name] == value)
-        elif control.isChecked():
+            ctrl.setChecked(cfg[name] == value)
+        elif ctrl.isChecked():
             cfg[name] = value
 
     else:
         log.error('Cannot handle widget "%s"!\n  type "%s" NOT covered yet!' %
-                  (name, type(control)))
+                  (name, type(ctrl)))
 
