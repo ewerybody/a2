4b7d2b0d96f56939fe3ce97b35d60f03593badbb
ewerybody <ewerybody@braincontrol.org>
2016-09-19 10:01:31 +0200

    wip on the element.template #84

--- lib/_defaults/element.template.py
+++ lib/_defaults/element.template.py
@@ -11,7 +11,8 @@ from a2element import DrawCtrl, EditCtrl
 
 class Draw(DrawCtrl):
     """
-    blaaaa
+    The frontend widget visible to the user with options to change the default\
+    behavior of the element.
     """
     def __init__(self, main, cfg, mod):
         super(Draw, self).__init__(main, cfg, mod)
@@ -20,7 +21,8 @@ class Draw(DrawCtrl):
 
 class Edit(EditCtrl):
     """
-    blaaa
+    The background widget that sets up how the user can edit the element, visible when
+    editing the module. 
     """
     def __init__(self, cfg, main, parentCfg):
         super(Edit, self).__init__(cfg, main, parentCfg, addLayout=False)
@@ -34,7 +36,30 @@ class Edit(EditCtrl):
 
 
 def get_settings(module_key, cfg, db_dict, user_cfg):
-    #db_dict.setdefault('variables', {})
-    #value = a2ctrl.get_cfg_value(cfg, user_cfg, typ=bool, default=False)
-    #db_dict['variables'][cfg['name']] = value
+    """
+    Called by the module on "change" to get an elements data thats
+    eventually written into the runtime includes.
+    
+    Passed into is all you might need:
+    :param str module_key: "module_source_name|module_name" combo used to identify module in db
+    :param dict cfg: Standard element configuration dictionary.
+    :param dict db_dict: Dictionary that's used to write the include data with "hotkeys", "variables" and "includes" keys
+    :param dict user_cfg: This elements user edits saved in the db
+    
+    To make changes to the:
+    * "variables" - a simple key, value dictionary in db_dict
+    
+    Get the current value via get_cfg_value() given the default cfg and user_cfg.
+    If value name is found it takes the value from there, otherwise from cfg or given default.
+    
+        value = a2ctrl.get_cfg_value(cfg, user_cfg, typ=bool, default=False)
+    
+    write the key and value to the "variables" dict:
+        
+        db_dict['variables'][cfg['name']] = value
+    
+    * "hotkeys" - a dictionary with scope identifiers
+    
+    * "includes" - a simple list with ahk script paths
+    """
     pass
