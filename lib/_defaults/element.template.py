36f447a6496b900a33308bb242945e86bf5a8165
ewerybody <ewerybody+github@gmail.com>
2016-09-23 15:26:51 +0200

    wip #84 create new element writes file and opens up explorer with it

--- lib/_defaults/element.template.py
+++ lib/_defaults/element.template.py
@@ -11,8 +11,8 @@ from a2element import DrawCtrl, EditCtrl
 
 class Draw(DrawCtrl):
     """
-    The frontend widget visible to the user with options to change the default\
-    behavior of the element.
+    The frontend widget visible to the user with options
+    to change the default behavior of the element.
     """
     def __init__(self, main, cfg, mod):
         super(Draw, self).__init__(main, cfg, mod)
@@ -21,13 +21,15 @@ class Draw(DrawCtrl):
 
 class Edit(EditCtrl):
     """
-    The background widget that sets up how the user can edit the element, visible when
-    editing the module. 
+    The background widget that sets up how the user can edit the element,
+    visible when editing the module.
     """
     def __init__(self, cfg, main, parentCfg):
         super(Edit, self).__init__(cfg, main, parentCfg, addLayout=False)
+
     @staticmethod
     def element_name():
+        """The elements display name shown in UI"""
         return '{element_name}'
 
     @staticmethod
@@ -39,27 +41,27 @@ def get_settings(module_key, cfg, db_dict, user_cfg):
     """
     Called by the module on "change" to get an elements data thats
     eventually written into the runtime includes.
-    
+
     Passed into is all you might need:
     :param str module_key: "module_source_name|module_name" combo used to identify module in db
     :param dict cfg: Standard element configuration dictionary.
     :param dict db_dict: Dictionary that's used to write the include data with "hotkeys", "variables" and "includes" keys
     :param dict user_cfg: This elements user edits saved in the db
-    
+
     To make changes to the:
     * "variables" - a simple key, value dictionary in db_dict
-    
+
     Get the current value via get_cfg_value() given the default cfg and user_cfg.
     If value name is found it takes the value from there, otherwise from cfg or given default.
-    
+
         value = a2ctrl.get_cfg_value(cfg, user_cfg, typ=bool, default=False)
-    
+
     write the key and value to the "variables" dict:
-        
+
         db_dict['variables'][cfg['name']] = value
-    
+
     * "hotkeys" - a dictionary with scope identifiers
-    
+
     * "includes" - a simple list with ahk script paths
     """
     pass
--- ui/a2widget/new_element_tool.py
+++ ui/a2widget/new_element_tool.py
@@ -5,15 +5,36 @@
 import os
 
 from a2widget.a2input_dialog import A2InputDialog
+import a2core
+import subprocess
 
 
 class NewElementDialog(A2InputDialog):
     def __init__(self, main):
         self.main = main
-        super(NewElementDialog, self).__init__(self.main, 'New Element Dialog')
-        template = os.path.join(self.main.a2.paths._defaults, 'element.template.py')
-        print('template: %s' % template)
+        super(NewElementDialog, self).__init__(
+            self.main, 'New Element Dialog', msg='Name the new element:', text='name',
+            okFunc=self.create_element,
+            checkFunc=self.check_element_name)
+        self._current_elements = [os.path.splitext(f)[0] for f in os.listdir(self.main.a2.paths.elements)]
 
+    def check_element_name(self, name):
+        return a2core.standard_name_check(name, self._current_elements)
+
+    def create_element(self, name):
+        template = os.path.join(self.main.a2.paths._defaults, 'element.template.py')
+        with open(template) as fobj:
+            new_element_code = fobj.read().format(
+                description='Some element description ...',
+                creation_date=a2core.get_date(),
+                author_name=self.main.devset.author_name,
+                element_name=name.title())
+
+        new_path = os.path.join(self.main.a2.paths.elements, name + '.py')
+        with open(new_path, 'w') as fobj:
+            fobj.write(new_element_code)
+
+        subprocess.Popen(['explorer.exe', '/select,', os.path.normpath(new_path)])
 
 
 if __name__ == '__main__':
