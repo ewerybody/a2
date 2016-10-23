dc386e97b813c1844d9b811f896fe725a2cf147d
ewerybody <ewerybody@braincontrol.org>
2016-10-24 00:27:59 +0200

    some wip on item_editor #103

--- ui/a2widget/a2item_editor.py
+++ ui/a2widget/a2item_editor.py
@@ -4,9 +4,14 @@ a2widget.a2item_editor
 @created: Sep 26, 2016
 @author: eRiC
 """
+import a2ctrl
 from PySide import QtGui
+from a2widget import a2item_editor_ui
 
 
 class A2ItemEditor(QtGui.QWidget):
     def __init__(self, *args, **kwargs):
         super(A2ItemEditor, self).__init__(*args, **kwargs)
+        a2ctrl.check_ui_module(a2item_editor_ui)
+        self.ui = a2item_editor_ui.Ui_A2ItemEditor()
+        self.ui.setupUi(self)
--- ui/a2widget/a2item_editor_ui.py
+++ ui/a2widget/a2item_editor_ui.py
@@ -2,34 +2,34 @@
 
 # Form implementation generated from reading ui file 'C:\Users\eRiC\io\code\a2\ui\a2widget\a2item_editor.ui'
 #
-# Created: Mon Sep 26 09:56:27 2016
+# Created: Mon Oct 24 00:04:38 2016
 #      by: pyside-uic 0.2.15 running on PySide 1.2.2
 #
 # WARNING! All changes made in this file will be lost!
 
 from PySide import QtCore, QtGui
 
-class Ui_Form(object):
-    def setupUi(self, Form):
-        Form.setObjectName("Form")
-        Form.resize(726, 808)
-        self.horizontalLayout_2 = QtGui.QHBoxLayout(Form)
+class Ui_A2ItemEditor(object):
+    def setupUi(self, A2ItemEditor):
+        A2ItemEditor.setObjectName("A2ItemEditor")
+        A2ItemEditor.resize(726, 731)
+        self.horizontalLayout_2 = QtGui.QHBoxLayout(A2ItemEditor)
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
         self.verticalLayout = QtGui.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
-        self.search_field = QtGui.QLineEdit(Form)
+        self.search_field = QtGui.QLineEdit(A2ItemEditor)
         self.search_field.setObjectName("search_field")
         self.verticalLayout.addWidget(self.search_field)
         self.horizontalLayout = QtGui.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.add_entry_button = QtGui.QPushButton(Form)
+        self.add_entry_button = QtGui.QPushButton(A2ItemEditor)
         self.add_entry_button.setObjectName("add_entry_button")
         self.horizontalLayout.addWidget(self.add_entry_button)
-        self.del_entry_button = QtGui.QPushButton(Form)
+        self.del_entry_button = QtGui.QPushButton(A2ItemEditor)
         self.del_entry_button.setObjectName("del_entry_button")
         self.horizontalLayout.addWidget(self.del_entry_button)
         self.verticalLayout.addLayout(self.horizontalLayout)
-        self.list = QtGui.QListView(Form)
+        self.list = QtGui.QListView(A2ItemEditor)
         self.list.setObjectName("list")
         self.verticalLayout.addWidget(self.list)
         self.horizontalLayout_2.addLayout(self.verticalLayout)
@@ -37,24 +37,25 @@ class Ui_Form(object):
         self.config_layout.setObjectName("config_layout")
         self.horizontalLayout_3 = QtGui.QHBoxLayout()
         self.horizontalLayout_3.setObjectName("horizontalLayout_3")
-        self.label = QtGui.QLabel(Form)
+        self.label = QtGui.QLabel(A2ItemEditor)
         self.label.setObjectName("label")
         self.horizontalLayout_3.addWidget(self.label)
-        self.entry_name = QtGui.QLineEdit(Form)
+        self.entry_name = QtGui.QLineEdit(A2ItemEditor)
         self.entry_name.setObjectName("entry_name")
         self.horizontalLayout_3.addWidget(self.entry_name)
         self.config_layout.addLayout(self.horizontalLayout_3)
         spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
         self.config_layout.addItem(spacerItem)
         self.horizontalLayout_2.addLayout(self.config_layout)
+        self.horizontalLayout_2.setStretch(1, 1)
 
-        self.retranslateUi(Form)
-        QtCore.QMetaObject.connectSlotsByName(Form)
+        self.retranslateUi(A2ItemEditor)
+        QtCore.QMetaObject.connectSlotsByName(A2ItemEditor)
 
-    def retranslateUi(self, Form):
-        Form.setWindowTitle(QtGui.QApplication.translate("Form", "Form", None, QtGui.QApplication.UnicodeUTF8))
-        self.search_field.setPlaceholderText(QtGui.QApplication.translate("Form", "search", None, QtGui.QApplication.UnicodeUTF8))
-        self.add_entry_button.setText(QtGui.QApplication.translate("Form", "Add Entry", None, QtGui.QApplication.UnicodeUTF8))
-        self.del_entry_button.setText(QtGui.QApplication.translate("Form", "Remove Entry", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("Form", "Entry Name", None, QtGui.QApplication.UnicodeUTF8))
+    def retranslateUi(self, A2ItemEditor):
+        A2ItemEditor.setWindowTitle(QtGui.QApplication.translate("A2ItemEditor", "Form", None, QtGui.QApplication.UnicodeUTF8))
+        self.search_field.setPlaceholderText(QtGui.QApplication.translate("A2ItemEditor", "search", None, QtGui.QApplication.UnicodeUTF8))
+        self.add_entry_button.setText(QtGui.QApplication.translate("A2ItemEditor", "Add Entry", None, QtGui.QApplication.UnicodeUTF8))
+        self.del_entry_button.setText(QtGui.QApplication.translate("A2ItemEditor", "Remove Entry", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("A2ItemEditor", "Entry Name", None, QtGui.QApplication.UnicodeUTF8))
 
