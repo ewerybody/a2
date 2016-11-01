8e3c0ea5da63a25904db8b4c9f4bbae91dbc08a0
ewerybody <ewerybody+github@gmail.com>
2016-11-01 22:48:30 +0100

    common widget classes now directly at a2widget.

---
+++ ui/a2widget/__init__.py
@@ -0,0 +1,6 @@
+from a2widget.a2hotkey import A2Hotkey
+from a2widget.a2input_dialog import A2InputDialog
+from a2widget.a2item_editor import A2ItemEditor
+from a2widget.a2path_field import A2PathField
+from a2widget.a2slider import A2Slider
+from a2widget.textfield_autoheight import TextField_AutoHeight
