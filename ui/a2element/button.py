5730abca00e638b5adff378ed9ccdb34fa0b4240
ewerybody <ewerybody+github@gmail.com>
2016-11-14 16:03:55 +0100

    finished #107 implemented button to call Python call from UI

---
+++ ui/a2element/button.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Some element description ...
+
+@created: 2016 11 14
+@author: Eric Werner
+"""
+import a2ctrl
+from PySide import QtGui
+from a2element import DrawCtrl, EditCtrl, button_edit_ui
+from a2core import get_logger
+import traceback
+
+
+log = get_logger(__name__)
+
+
+class Draw(DrawCtrl):
+    """
+    The frontend widget visible to the user with options
+    to change the default behavior of the element.
+    """
+    def __init__(self, main, cfg, mod):
+        super(Draw, self).__init__(main, cfg, mod)
+
+        self.button_layout = QtGui.QHBoxLayout(self)
+        labeltext = self.cfg.get('labeltext', '')
+        if labeltext:
+            label = QtGui.QLabel(labeltext)
+            self.button_layout.addWidget(label)
+
+        self.button = QtGui.QPushButton(self.cfg.get('buttontext', ''))
+        self.button.clicked.connect(self.call_code)
+        self.button_layout.addWidget(self.button)
+        # self.setLayout(self.main_layout)
+
+    def call_code(self):
+        code = self.cfg.get('code', '')
+        if not code:
+            raise RuntimeError('Button has no code to execute!')
+        else:
+            try:
+                exec(code)
+            except Exception:
+                log.error(traceback.format_exc().strip())
+                log.error('Failed to call button code in "%s":\n  %s'
+                          % (self.mod.name, code))
+
+
+class Edit(EditCtrl):
+    """
+    The background widget that sets up how the user can edit the element,
+    visible when editing the module.
+    """
+    def __init__(self, cfg, main, parentCfg):
+        super(Edit, self).__init__(cfg, main, parentCfg, add_layout=False)
+
+        a2ctrl.check_ui_module(button_edit_ui)
+        self.ui = button_edit_ui.Ui_edit()
+        self.ui.setupUi(self.mainWidget)
+        a2ctrl.connect.cfg_controls(self.cfg, self.ui)
+
+    @staticmethod
+    def element_name():
+        """The elements display name shown in UI"""
+        return 'Button'
+
+    @staticmethod
+    def element_icon():
+        return a2ctrl.Icons.inst().check
+
+
+def get_settings(module_key, cfg, db_dict, user_cfg):
+    pass
---
+++ ui/a2element/button_edit_ui.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\eric\io\code\a2\ui\a2element\button_edit.ui'
+#
+# Created: Mon Nov 14 14:37:15 2016
+#      by: pyside-uic 0.2.15 running on PySide 1.2.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore, QtGui
+
+class Ui_edit(object):
+    def setupUi(self, edit):
+        edit.setObjectName("edit")
+        edit.resize(607, 296)
+        self.edit_layout = QtGui.QFormLayout(edit)
+        self.edit_layout.setFieldGrowthPolicy(QtGui.QFormLayout.AllNonFixedFieldsGrow)
+        self.edit_layout.setLabelAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.edit_layout.setObjectName("edit_layout")
+        self.displayLabelLabel = QtGui.QLabel(edit)
+        self.displayLabelLabel.setMinimumSize(QtCore.QSize(100, 0))
+        self.displayLabelLabel.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.displayLabelLabel.setObjectName("displayLabelLabel")
+        self.edit_layout.setWidget(0, QtGui.QFormLayout.LabelRole, self.displayLabelLabel)
+        self.cfg_labeltext = QtGui.QLineEdit(edit)
+        self.cfg_labeltext.setText("")
+        self.cfg_labeltext.setPlaceholderText("space used by button if no label text")
+        self.cfg_labeltext.setObjectName("cfg_labeltext")
+        self.edit_layout.setWidget(0, QtGui.QFormLayout.FieldRole, self.cfg_labeltext)
+        self.displayLabelLabel_2 = QtGui.QLabel(edit)
+        self.displayLabelLabel_2.setMinimumSize(QtCore.QSize(100, 0))
+        self.displayLabelLabel_2.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.displayLabelLabel_2.setObjectName("displayLabelLabel_2")
+        self.edit_layout.setWidget(1, QtGui.QFormLayout.LabelRole, self.displayLabelLabel_2)
+        self.cfg_buttontext = QtGui.QLineEdit(edit)
+        self.cfg_buttontext.setText("")
+        self.cfg_buttontext.setObjectName("cfg_buttontext")
+        self.edit_layout.setWidget(1, QtGui.QFormLayout.FieldRole, self.cfg_buttontext)
+        self.displayLabelLabel_3 = QtGui.QLabel(edit)
+        self.displayLabelLabel_3.setMinimumSize(QtCore.QSize(100, 0))
+        self.displayLabelLabel_3.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.displayLabelLabel_3.setObjectName("displayLabelLabel_3")
+        self.edit_layout.setWidget(2, QtGui.QFormLayout.LabelRole, self.displayLabelLabel_3)
+        self.cfg_code = a2CodeField(edit)
+        self.cfg_code.setObjectName("cfg_code")
+        self.edit_layout.setWidget(2, QtGui.QFormLayout.FieldRole, self.cfg_code)
+
+        self.retranslateUi(edit)
+        QtCore.QMetaObject.connectSlotsByName(edit)
+
+    def retranslateUi(self, edit):
+        edit.setWindowTitle(QtGui.QApplication.translate("edit", "Form", None, QtGui.QApplication.UnicodeUTF8))
+        self.displayLabelLabel.setText(QtGui.QApplication.translate("edit", "label text:", None, QtGui.QApplication.UnicodeUTF8))
+        self.displayLabelLabel_2.setText(QtGui.QApplication.translate("edit", "button text:", None, QtGui.QApplication.UnicodeUTF8))
+        self.cfg_buttontext.setPlaceholderText(QtGui.QApplication.translate("edit", "text to show on the button", None, QtGui.QApplication.UnicodeUTF8))
+        self.displayLabelLabel_3.setText(QtGui.QApplication.translate("edit", "Python Code:", None, QtGui.QApplication.UnicodeUTF8))
+
+from a2widget import a2CodeField
