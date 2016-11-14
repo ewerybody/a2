4a3c612666e5833e6ac071e1b68818307a49364c
ewerybody <ewerybody+github@gmail.com>
2016-11-14 11:08:49 +0100

    mod change now cleans up init_calls when removed

--- ui/a2mod.py
+++ ui/a2mod.py
@@ -188,7 +188,7 @@ class Mod(object):
         """
         Sets the mods own db entries
         """
-        db_dict = {'variables': {}, 'hotkeys': {}, 'includes': []}
+        db_dict = {'variables': {}, 'hotkeys': {}, 'includes': [], 'init_calls': []}
         a2ctrl.assemble_settings(self.key, self.config[1:], db_dict, self.path)
         for typ, data in db_dict.items():
             self.a2.db.set(typ, data, self.key)
