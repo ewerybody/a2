d1e3ce6a9adc99505f8e5971b3abb65b7367b674
ewerybody <ewerybody+github@gmail.com>
2017-04-12 10:40:02 +0200

    renamed a2text_field module more consistently

--- ui/a2widget/__init__.py
+++ ui/a2widget/__init__.py
@@ -3,7 +3,7 @@ from a2widget.a2input_dialog import A2InputDialog
 from a2widget.a2item_editor import A2ItemEditor
 from a2widget.a2path_field import A2PathField
 from a2widget.a2slider import A2Slider
-from a2widget.a2textfield import A2TextField, A2CodeField
+from a2widget.a2text_field import A2TextField, A2CodeField
 from a2widget.a2coords_field import A2CoordsField
 from a2widget.a2button_field import A2ButtonField
 from a2widget.a2tag_field import A2TagField
