1f72ba71f06aa13ec1ce4fb1b64cc27df914ac16
ewerybody <ewerybody+github@gmail.com>
2017-08-25 14:49:49 +0200

    improved coords field demo

--- ui/a2widget/demo/a2coords_field.py
+++ ui/a2widget/demo/a2coords_field.py
@@ -17,26 +17,27 @@ class CoordsFieldDemo(QtGui.QMainWindow):
         super(CoordsFieldDemo, self).__init__()
         w = QtGui.QWidget(self)
         self.setCentralWidget(w)
-        l = QtGui.QVBoxLayout(w)
-        w.setLayout(l)
+        vlay = QtGui.QVBoxLayout(w)
 
         self.c = A2CoordsField()
         self.c.changed.connect(self.change_received)
-        l.addWidget(self.c)
 
         self.c2 = A2CoordsField()
-        l.addWidget(self.c2)
 
         self.show_current_timer = QtCore.QTimer()
-        self.show_current_timer.setInterval(50)
+        self.show_current_timer.setInterval(100)
         self.show_current_timer.timeout.connect(self.show_current_pos)
         self.show_current_timer.start()
 
         self.some_dict = {'some_coords': (23, 42), 'something_else': 'blaaa'}
         self.c3 = A2CoordsField()
-        l.addWidget(self.c3)
         a2ctrl.connect.control(self.c3, 'some_coords', self.some_dict, self.dict_changed)
         self.dict_changed.connect(self.show_dict_change)
+
+        for l, w in [('Simple field:', self.c), ('Constantly updated:', self.c2),
+                     ('Dictionary connected:', self.c3)]:
+            vlay.addWidget(QtGui.QLabel(l))
+            vlay.addWidget(w)
 
     def show_current_pos(self):
         self.c2.set_value(QtGui.QCursor.pos())
@@ -53,6 +54,7 @@ def show():
     win = CoordsFieldDemo()
     win.show()
     app.exec_()
+
 
 if __name__ == '__main__':
     show()
