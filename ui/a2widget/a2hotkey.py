39a940af475a573a9b9a53de6e8ced84bdee064c
ewerybody <ewerybody+github@gmail.com>
2017-08-30 15:19:55 +0200

    :hammer: some wip on the new hotkey dialog #15

--- ui/a2widget/a2hotkey.py
+++ ui/a2widget/a2hotkey.py
@@ -4,6 +4,8 @@ from PySide import QtGui, QtCore
 
 import a2ahk
 import a2core
+import a2ctrl
+import a2widget.keyboard.base_ui
 
 
 log = a2core.get_logger(__name__)
@@ -11,6 +13,14 @@ LEAVE_CLOSE_TIMEOUT = 1000
 STYLE_BAD = '* {color:#F00}'
 STYLE_GOOD = '* {color:#0F0}'
 _dialog_size = None
+
+
+STYLE_BUTTON = """QPushButton {
+    padding: 0px;
+    min-width: 1px;
+}"""
+#    padding-left: 0px;
+#    padding-right: 0px;
 
 
 class A2Hotkey(QtGui.QPushButton):
@@ -33,15 +43,116 @@ class A2Hotkey(QtGui.QPushButton):
                 self.ok_func(key)
 
     def popup_dialog(self):
-        dialog = HotkeyDialog(self)
+        dialog = HotkeyDialog1(self)
         dialog.hotkey_set.connect(self.set_key)
+        dialog.show()
 
 
-class HotkeyDialog(QtGui.QWidget):
+class HotkeyDialog2(QtGui.QDialog):
     hotkey_set = QtCore.Signal(str)
 
     def __init__(self, parent):
-        super(HotkeyDialog, self).__init__(parent)
+        super(HotkeyDialog2, self).__init__(parent)
+        self.parent_widget = parent
+        self.setModal(True)
+        self.a2 = a2core.A2Obj.inst()
+        a2ctrl.check_ui_module(a2widget.keyboard.base_ui)
+        self.setup_ui()
+#        QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Enter), self, self.ok)
+#        QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Escape), self, self.close)
+#
+#        self.setWindowFlags(QtCore.Qt.Window | QtCore.Qt.CustomizeWindowHint)
+#
+#        self.key = self.parent_widget.key
+#        self.okay_state = True
+#        self.validate_hotkey(self.key)
+#
+        self.place_at_cursor()
+#        self.show()
+        self._init_dialog_size()
+#
+#        hotkeys = self.a2.get_used_hotkeys()
+#        pprint(hotkeys)
+
+    def setup_ui(self):
+        self.ui = a2widget.keyboard.base_ui.Ui_Keyboard()
+        self.ui.setupUi(self)
+        self.ui.keys_widget.setStyleSheet(STYLE_BUTTON)
+
+        self.ui.left_key.setText('←')
+        self.ui.up_key.setText('↑')
+        self.ui.down_key.setText('↓')
+        self.ui.right_key.setText('→')
+
+    def ok(self):
+        log.info('key: %s' % self.key)
+        log.info('ok: %s' % self.okay_state)
+        if self.okay_state:
+            self.hotkey_set.emit(self.key)
+            self.close()
+
+    def validate_hotkey(self, hk_string):
+        """
+        first implementation: checks for valid modifiers + a single key
+        TODO: handle F1-F12, Del, Home...
+              handle single keys when in scope,
+              check availability ...
+        """
+        okay_state = False
+        hk_parts = hk_string.split('+')
+        key = hk_parts[-1].strip().lower()
+        modifier = []
+        tilde = ''
+        # TODO: implement check for joystick keys and scancodes: 2joy4, SCnnn
+        # http://www.autohotkey.com/docs/KeyList.htm#SpecialKeys
+        if len(key) != 1 and key not in a2ahk.keys:
+            msg = 'Invalid key! (%s)' % key
+        elif len(hk_parts) == 1:
+            okay_state = True
+        else:
+            modifier = [k.strip().lower() for k in hk_parts[:-1]]
+            if modifier[0].startswith('~'):
+                tilde = '~'
+                modifier[0] = modifier[0][1:]
+            bad_modifier = [k for k in modifier if k not in a2ahk.modifiers]
+            if bad_modifier:
+                msg = ('Modifyer not one of Win, Ctrl, Alt or Shift! (%s)' % ', '.join(bad_modifier))
+            else:
+                okay_state = True
+
+        if not okay_state:
+            self.text_edit.setStyleSheet(STYLE_BAD)
+            log.error(msg)
+        else:
+            modifier = [k.title() for k in modifier]
+            key = key.title()
+            self.key = tilde + '+'.join(modifier + [key])
+            log.info('tempKey %s:' % self.key)
+            self.text_edit.setStyleSheet(STYLE_GOOD)
+
+        self.okay_state = okay_state
+
+    def place_at_cursor(self):
+        cursor_pos = QtGui.QCursor.pos()
+        pos = self.pos()
+        dialog_size = _dialog_size if _dialog_size else (200, 100)
+        pos.setX(cursor_pos.x() - (dialog_size[0] / 2))
+        pos.setY(cursor_pos.y() - (dialog_size[1] / 2))
+        self.move(pos)
+
+    def closeEvent(self, event):
+        return QtGui.QWidget.closeEvent(self, event)
+
+    def _init_dialog_size(self):
+        global _dialog_size
+        _dialog_size = (self.width(), self.height())
+
+
+class HotkeyDialog1(QtGui.QWidget):
+    hotkey_set = QtCore.Signal(str)
+
+    def __init__(self, parent):
+        super(HotkeyDialog1, self).__init__(parent)
         self.parent_widget = parent
         self.a2 = a2core.A2Obj.inst()
         self.setup_ui()
