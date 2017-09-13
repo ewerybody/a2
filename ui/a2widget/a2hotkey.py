69f42ef9027afdc3d0f7ee9b170a78ab03e7f992
ewerybody <ewerybody+github@gmail.com>
2017-09-13 20:10:35 +0200

    moved Hotkey Keyboard dialog classes

--- ui/a2widget/a2hotkey.py
+++ ui/a2widget/a2hotkey.py
@@ -6,6 +6,7 @@ import a2ahk
 import a2core
 import a2ctrl
 import a2runtime
+from a2widget.keyboard.base import KeyboardDialogBase
 import a2widget.keyboard.base_ui
 
 
@@ -23,8 +24,6 @@ STYLE_BUTTON = """
         font-size: 12px;
     }
     """
-#    padding-left: 0px;
-#    padding-right: 0px;
 
 
 class A2Hotkey(QtGui.QPushButton):
@@ -44,7 +43,7 @@ class A2Hotkey(QtGui.QPushButton):
         self.scope_data = scope_data
         self.clicked.connect(self.popup_dialog)
 
-        self.dialog_styles = [HotkeyDialog2]
+        self.dialog_styles = [HotKeyBoard]
         self.dialog_default = HotkeyDialog1
         self.dialog_styles.append(self.dialog_default)
 
@@ -68,143 +67,36 @@ class A2Hotkey(QtGui.QPushButton):
         dialog.show()
 
 
-class HotkeyDialog2(QtGui.QDialog):
+class _HotkeyBase(QtCore.QObject):
     hotkey_set = QtCore.Signal(str)
+    label = ''
+
+    def __init__(self, parent, key, scope_data=None):
+        self.parent = parent
+        self.key = key
+        self.scope_data = scope_data
+        self.a2 = a2core.A2Obj.inst()
+
+
+class HotKeyBoard(KeyboardDialogBase, _HotkeyBase):
     label = 'Hotkey Keyboard'
 
     def __init__(self, parent, key, scope_data=None):
-        super(HotkeyDialog2, self).__init__(parent)
-        self.key = key
-        self.scope_data = scope_data
-        self.setModal(True)
-        self.a2 = a2core.A2Obj.inst()
+        KeyboardDialogBase.__init__(self, parent)
+        _HotkeyBase.__init__(self, parent, key, scope_data)
 
-        self.setup_ui()
-        self.place_at_cursor()
-        self._init_dialog_size()
-
-    def setup_ui(self):
-        a2ctrl.check_ui_module(a2widget.keyboard.base_ui)
-        self.ui = a2widget.keyboard.base_ui.Ui_Keyboard()
-        self.ui.setupUi(self)
-        self.ui.keys_widget.setStyleSheet(STYLE_BUTTON)
-
-        self.ui.left_key.setText('←')
-        self.ui.up_key.setText('↑')
-        self.ui.down_key.setText('↓')
-        self.ui.right_key.setText('→')
-
-        for i in range(1, 13):
-            self.add_key('f%i_key' % i, 'F%i' % i, self.ui.f_row)
-
-        # TODO: This needs to be subclassed for an english keyboard
-        self.add_key('tick_key', '`', self.ui.number_row, 0)
-        for i in range(1, 10):
-            self.add_key('digit%i_key' % i, str(i), self.ui.number_row, i)
-        for i, l, name in [(10, '0', 'digit0'), (11, '-', 'minus'), (12, '=', 'equals')]:
-            self.add_key('%s_key' % name, l, self.ui.number_row, i)
-
-        for l in 'qwertyuiop':
-            self.add_key('%s_key' % l, l, self.ui.letter_row_top)
-        self.add_key('bracket_open_key', '[', self.ui.letter_row_top)
-        self.add_key('bracket_close_key', ']', self.ui.letter_row_top)
-        self.add_key('backslash_key', '\\', self.ui.letter_row_top)
-
-        for l in 'asdfghjkl':
-            self.add_key('%s_key' % l, l, self.ui.letter_row_middle)
-        self.add_key('semicolon_key', ';', self.ui.letter_row_middle)
-        self.add_key('quote_key', "'", self.ui.letter_row_middle)
-        self.add_key('return_key', "Enter", self.ui.letter_row_middle)
-
-        letters = 'zxcvbnm'
-        for i in range(len(letters)):
-            self.add_key('%s_key' % letters[i], letters[i], self.ui.letter_row_bottom, i + 1)
-        for i, l, name in [(8, ',', 'comma'), (9, '.', 'dot'), (10, '/', 'slash')]:
-            self.add_key('%s_key' % name, l, self.ui.letter_row_bottom, i)
-
-    def add_key(self, name, label, layout, index=None):
-        button = QtGui.QPushButton(self.ui.keys_widget)
-        button.setObjectName(name)
-        button.setText(label)
-        if index is None:
-            layout.addWidget(button)
-        else:
-            layout.insertWidget(index, button)
-        setattr(self.ui, name, button)
-
-    def ok(self):
-        log.info('key: %s' % self.key)
-        log.info('ok: %s' % self.okay_state)
-        if self.okay_state:
-            self.hotkey_set.emit(self.key)
-            self.close()
-
-    def validate_hotkey(self, hk_string):
-        """
-        first implementation: checks for valid modifiers + a single key
-        TODO: handle F1-F12, Del, Home...
-              handle single keys when in scope,
-              check availability ...
-        """
-        okay_state = False
-        hk_parts = hk_string.split('+')
-        key = hk_parts[-1].strip().lower()
-        modifier = []
-        tilde = ''
-        # TODO: implement check for joystick keys and scancodes: 2joy4, SCnnn
-        # http://www.autohotkey.com/docs/KeyList.htm#SpecialKeys
-        if len(key) != 1 and key not in a2ahk.keys:
-            msg = 'Invalid key! (%s)' % key
-        elif len(hk_parts) == 1:
-            okay_state = True
-        else:
-            modifier = [k.strip().lower() for k in hk_parts[:-1]]
-            if modifier[0].startswith('~'):
-                tilde = '~'
-                modifier[0] = modifier[0][1:]
-            bad_modifier = [k for k in modifier if k not in a2ahk.modifiers]
-            if bad_modifier:
-                msg = ('Modifyer not one of Win, Ctrl, Alt or Shift! (%s)' % ', '.join(bad_modifier))
-            else:
-                okay_state = True
-
-        if not okay_state:
-            self.text_edit.setStyleSheet(STYLE_BAD)
-            log.error(msg)
-        else:
-            modifier = [k.title() for k in modifier]
-            key = key.title()
-            self.key = tilde + '+'.join(modifier + [key])
-            log.info('tempKey %s:' % self.key)
-            self.text_edit.setStyleSheet(STYLE_GOOD)
-
-        self.okay_state = okay_state
-
-    def place_at_cursor(self):
-        cursor_pos = QtGui.QCursor.pos()
-        pos = self.pos()
-        dialog_size = _dialog_size if _dialog_size else (200, 100)
-        pos.setX(cursor_pos.x() - (dialog_size[0] / 2))
-        pos.setY(cursor_pos.y() - (dialog_size[1] / 2))
-        self.move(pos)
-
-    def closeEvent(self, event):
-        return QtGui.QWidget.closeEvent(self, event)
-
-    def _init_dialog_size(self):
-        global _dialog_size
-        _dialog_size = (self.width(), self.height())
+        keyboard_id = self.a2.db.get('keyboard_id') or 'en_us'
+        self.build_keyboad(keyboard_id)
 
 
-class HotkeyDialog1(QtGui.QWidget):
-    hotkey_set = QtCore.Signal(str)
+class HotkeyDialog1(QtGui.QWidget, _HotkeyBase):
     label = 'Simple Dialog'
 
     def __init__(self, parent, key, scope_data=None):
-        super(HotkeyDialog1, self).__init__(parent)
-        self.key = key
-        self.scope_data = scope_data
-        self.a2 = a2core.A2Obj.inst()
+        # super(HotkeyDialog1, self).__init__(parent)
+        QtGui.QWidget.__init__(self, parent)
+        _HotkeyBase.__init__(self, parent, key, scope_data)
+
         self.setup_ui()
 
         QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Enter), self, self.ok)
---
+++ ui/a2widget/keyboard/base.py
@@ -0,0 +1,105 @@
+"""
+Created on 11.09.2017
+
+@author: eric
+"""
+from enum import Enum
+
+import a2ctrl
+
+import a2widget.keyboard.base_ui
+from PySide import QtGui, QtCore
+
+
+STYLE_BUTTON = """
+    QPushButton {
+        padding: 3px;
+        min-width: 1px;
+        font-size: 12px;
+    }
+    """
+#    padding-left: 0px;
+#    padding-right: 0px;
+
+
+class KeyboardDialogBase(QtGui.QDialog):
+    def __init__(self, parent):
+        super(KeyboardDialogBase, self).__init__(parent)
+        self.setModal(True)
+
+        self.keydict = {}
+        self.modifier = {}
+
+        self._setup_ui()
+        self._fill_keydict()
+
+    def _setup_ui(self):
+        a2ctrl.check_ui_module(a2widget.keyboard.base_ui)
+        self.ui = a2widget.keyboard.base_ui.Ui_Keyboard()
+        self.ui.setupUi(self)
+
+        self.ui.keys_widget.setStyleSheet(STYLE_BUTTON)
+
+        self.ui.left_key.setText('←')
+        self.ui.up_key.setText('↑')
+        self.ui.down_key.setText('↓')
+        self.ui.right_key.setText('→')
+
+        for i in range(1, 13):
+            self.add_key('f%i' % i, self.ui.f_row)
+
+        for i in range(0, 10):
+            self.insert_key(i, str(i), self.ui.number_row)
+        # for i, l, name in [(10, '0', 'digit0'), (11, '-', 'minus'), (12, '=', 'equals')]:
+        for i, k in [(10, '0'), (11, '-'), (12, '=')]:
+            self.insert_key(i, k, self.ui.number_row)
+
+    def insert_key(self, index, key, layout, label=None, tooltip=None):
+        button = self._create_key(key, label, tooltip)
+        layout.insertWidget(index, button)
+
+    def add_key(self, key, layout, label=None, tooltip=None):
+        button = self._create_key(key, label, tooltip)
+        layout.addWidget(button)
+
+    def _create_key(self, key, label, tooltip):
+        """
+        A key can have different things:
+        * printed on it
+        * as dictionary name
+        #* as an internal name
+        * showing as a tooltip
+        """
+        # name = key + '_key'
+        button = QtGui.QPushButton(self.ui.keys_widget)
+        # button.setObjectName(name)
+        # setattr(self.ui, name, button)
+
+        if label:
+            button.setText(label)
+        else:
+            button.setText(key.upper())
+
+        if tooltip:
+            button.setToolTip(tooltip)
+
+        self.keydict[key] = button
+        return button
+
+    def _fill_keydict(self):
+        self.keydict['left'] = self.ui.left_key
+        self.keydict['up'] = self.ui.up_key
+        self.keydict['down'] = self.ui.down_key
+        self.keydict['right'] = self.ui.right_key
+
+        for modkeyname in ['alt', 'ctrl', 'shift', 'win']:
+            self.keydict[modkeyname] = []
+            for side in 'lr':
+                button = getattr(self.ui, '%s_%s_key' % (side, modkeyname))
+                self.keydict[side + modkeyname] = button
+                self.keydict[modkeyname].append(button)
+
+    def build_keyboad(self, keyboard_id):
+        if keyboard_id == 'en_us':
+            import a2widget.keyboard.en_us
+            a2widget.keyboard.en_us.main(self)
