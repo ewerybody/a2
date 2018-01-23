31c36fd525c8576a899cdc0278c590f711626154
Eric Werner <ewerybody+github@gmail.com>
2018-01-23 22:48:43 +0100

    wip #15 separated css and widgets

--- ui/a2widget/keyboard/base.py
+++ ui/a2widget/keyboard/base.py
@@ -3,31 +3,22 @@ Created on 11.09.2017
 
 @author: eric
 """
+import os
 import a2ahk
 import a2ctrl
 import a2core
-import a2widget.keyboard.base_ui
+from a2widget.keyboard import base_ui, mouse_ui, numpad_ui
+
 from PySide import QtGui, QtCore
 
-log = a2core.get_logger('keyboard_base')
 
+log = a2core.get_logger('keyboard_base')
 BASE_MODIFIERS = ['alt', 'ctrl', 'shift', 'win']
 SIDES = 'lr'
-KEYFONT_SIZE_FACTOR = 0.8
-STYLE_BUTTON = """
-    QPushButton {
-        font-size: %(font-size)ipx;
-        padding: %(padding)ipx;
-        min-width: 15px;
-        min-height: 22px;
-        max-height: 22px;
-    }
-    QPushButton:checked {
-        background-color: "%(color)s";
-    }
-    """
-#    padding-left: 0px;
-#    padding-right: 0px;
+KEYFONT_SIZE_FACTOR = 0.5
+KEYFONT_SIZE_FACTOR_SMALL = 0.2
+DB_KEY_MOUSE = 'hotkey_dialog_show_mouse'
+DB_KEY_NUMPAD = 'hotkey_dialog_show_numpad'
 
 
 class KeyboardDialogBase(QtGui.QDialog):
@@ -37,13 +28,21 @@ class KeyboardDialogBase(QtGui.QDialog):
         self.setModal(True)
 
         self.a2 = a2core.A2Obj.inst()
-        self.keydict = {}
+        self.key_dict = {}
         self.modifier = {}
         self.checked_key = None
         self.checked_modifier = []
 
+        a2ctrl.check_ui_module(base_ui)
+        a2ctrl.check_ui_module(mouse_ui)
+        a2ctrl.check_ui_module(numpad_ui)
+        self.ui = base_ui.Ui_Keyboard()
+        self.ui.setupUi(self)
+        self.ui.num_block_widget = numpad_ui.Ui_Numpad()
+        self.ui.num_block_widget.setupUi(self)
+
+        self._fill_key_dict()
         self._setup_ui()
-        self._fill_keydict()
 
         self._toggle_numpad()
         self._toggle_mouse()
@@ -55,7 +54,7 @@ class KeyboardDialogBase(QtGui.QDialog):
         keys = self.key.lower().split('+')
         buttons = []
         for k in keys:
-            button = self.keydict.get(k)
+            button = self.key_dict.get(k)
             if button:
                 buttons.append(button)
             elif k in BASE_MODIFIERS:
@@ -70,31 +69,32 @@ class KeyboardDialogBase(QtGui.QDialog):
         self.ui.key_field.setText(self.key)
 
     def _setup_ui(self):
-        a2ctrl.check_ui_module(a2widget.keyboard.base_ui)
-        self.ui = a2widget.keyboard.base_ui.Ui_Keyboard()
-        self.ui.setupUi(self)
-
-        global STYLE_BUTTON
         scale = self.a2.win.css_values['scale']
-        print('scale: %i' % scale)
         color = self.a2.win.css_values['color_yellow']
-
         font_size = self.a2.win.css_values['font_size'] * KEYFONT_SIZE_FACTOR
-        STYLE_BUTTON = STYLE_BUTTON % {'font-size': font_size, 'color': color, 'padding': scale * 3}
-        STYLE_BUTTON += '\nQPushButton#_mouse_body {min-height: %ipx;}' % (30 * scale)
-        STYLE_BUTTON += '\nQPushButton#lbutton {min-height: %ipx;}' % (50 * scale)
-        STYLE_BUTTON += '\nQPushButton#wheelleft {min-height: %ipx;max-width: %ipx;}' % (50 * scale, 5 * scale)
-        STYLE_BUTTON += '\nQPushButton#wheelright {min-height: %ipx;max-width: %ipx;}' % (50 * scale, 5 * scale)
-        STYLE_BUTTON += '\nQPushButton#mbutton {min-height: %ipx;}' % (30 * scale)
-        STYLE_BUTTON += '\nQPushButton#rbutton {min-height: %ipx;}' % (50 * scale)
-        self.ui.keys_widget.setStyleSheet(STYLE_BUTTON)
+        font_size_small = self.a2.win.css_values['font_size'] * KEYFONT_SIZE_FACTOR_SMALL
+        key_height = scale * 25
 
-        self.ui.mouse_layout_1.setSpacing(scale * 2)
+        css = load_css('main')
+        css = css.format(font_size=font_size, color=color, font_size_small=font_size_small,
+                         key_height=key_height)
+
+        # for key_name in ['pgup', 'pgdn', 'scrolllock', 'printscreen']:
+        #     self.key_dict[key_name].setMaximumHeight(key_height)
+
+        self.ui.mouse_block_widget = MouseWidget(self)
+        self.ui.keys_layout.addWidget(self.ui.mouse_block_widget)
+        self.ui.mouse_block_widget.set_style(scale)
+
+        self.ui.keys_layout.setSpacing(scale * 20)
+        self.ui.main_block.setSpacing(scale * 5)
+        self.ui.cursor_block.setSpacing(scale * 5)
 
         self.ui.left.setText('←')
         self.ui.up.setText('↑')
         self.ui.down.setText('↓')
         self.ui.right.setText('→')
+        self.ui.keys_widget.setStyleSheet(css)
 
         for i in range(1, 13):
             self.add_key('f%i' % i, self.ui.f_row)
@@ -140,14 +140,17 @@ class KeyboardDialogBase(QtGui.QDialog):
         if tooltip:
             button.setToolTip(tooltip)
 
-        self.keydict[key] = button
+        self.key_dict[key] = button
         button.a2key = key
         button.clicked.connect(self._key_press)
         return button
 
-    def _key_press(self, button=None, update_label=True):
+    def _key_press(self, button=None, update_hotkey_label=True):
         if button is None:
             button = self.sender()
+
+        height = button.height()
+        print('height: %s' % height)
 
         # modifiers can be toggled however you like
         if button.a2key in self.modifier:
@@ -159,20 +162,20 @@ class KeyboardDialogBase(QtGui.QDialog):
         # there always has to be a trigger key tho:
         else:
             if button.a2key == self.checked_key:
-                self.keydict[button.a2key].setChecked(True)
+                self.key_dict[button.a2key].setChecked(True)
             else:
                 if self.checked_key is not None:
-                    self.keydict[self.checked_key].setChecked(False)
+                    self.key_dict[self.checked_key].setChecked(False)
                 self.checked_key = button.a2key
 
-        if update_label:
+        if update_hotkey_label:
             new_key_label = []
             handled = []
             for modkeyname in self.checked_modifier:
                 if modkeyname in handled:
                     continue
 
-                modkey = self.keydict[modkeyname]
+                modkey = self.key_dict[modkeyname]
                 if modkey.a2buddy in self.checked_modifier:
                     handled.append(modkey.a2buddy)
                     new_key_label.append(modkey.a2modifier)
@@ -183,30 +186,30 @@ class KeyboardDialogBase(QtGui.QDialog):
             self.key = '+'.join([k.title() for k in new_key_label])
             self.ui.key_field.setText(self.key)
 
-    def _fill_keydict(self):
+    def _fill_key_dict(self):
         for modkeyname in BASE_MODIFIERS:
             for i in [0, 1]:
                 side = SIDES[i]
-                keyname = side + modkeyname
-                button = getattr(self.ui, keyname)
+                key_name = side + modkeyname
+                button = getattr(self.ui, key_name)
                 button.setCheckable(True)
                 button.clicked.connect(self._key_press)
-                button.a2key = keyname
+                button.a2key = key_name
                 button.a2modifier = modkeyname
                 button.a2buddy = SIDES[int(not i)] + modkeyname
 
-                self.keydict[keyname] = button
-                self.modifier[keyname] = button
+                self.key_dict[key_name] = button
+                self.modifier[key_name] = button
 
-        for keyname in a2ahk.keys:
+        for key_name in a2ahk.keys:
             try:
-                obj = getattr(self.ui, keyname)
+                obj = getattr(self.ui, key_name)
                 if isinstance(obj, QtGui.QPushButton):
-                    if keyname not in self.keydict:
-                        obj.a2key = keyname
+                    if key_name not in self.key_dict:
+                        obj.a2key = key_name
                         obj.setCheckable(True)
                         obj.clicked.connect(self._key_press)
-                        self.keydict[keyname] = obj
+                        self.key_dict[key_name] = obj
             except AttributeError:
                 pass
 
@@ -217,7 +220,7 @@ class KeyboardDialogBase(QtGui.QDialog):
             obj = getattr(self.ui, objname)
             if not isinstance(obj, QtGui.QPushButton):
                 continue
-            if objname not in self.keydict:
+            if objname not in self.key_dict:
                 log.error('NOT IN!: %s' % objname)
 
     def build_keyboard(self, keyboard_id):
@@ -227,16 +230,32 @@ class KeyboardDialogBase(QtGui.QDialog):
 
     def _toggle_numpad(self, state=None):
         if state is None:
-            state = self.a2.db.get('hotkey_dialog_show_numpad') or False
+            state = self.a2.db.get(DB_KEY_NUMPAD) or False
             self.ui.check_numpad.setChecked(state)
-
-        self.ui.num_block_widget.setVisible(state)
-        self.a2.db.set('hotkey_dialog_show_numpad', state)
+        #self.ui.num_block_widget.setVisible(state)
+        self.a2.db.set(DB_KEY_NUMPAD, state)
 
     def _toggle_mouse(self, state=None):
         if state is None:
-            state = self.a2.db.get('hotkey_dialog_show_mouse') or False
+            state = self.a2.db.get(DB_KEY_MOUSE) or False
             self.ui.check_mouse.setChecked(state)
-
         self.ui.mouse_block_widget.setVisible(state)
-        self.a2.db.set('hotkey_dialog_show_mouse', state)
+        self.a2.db.set(DB_KEY_MOUSE, state)
+
+
+class MouseWidget(QtGui.QWidget):
+    def __init__(self, parent):
+        super(MouseWidget, self).__init__(parent)
+        self.ui = mouse_ui.Ui_Mouse()
+        self.ui.setupUi(self)
+
+    def set_style(self, scale):
+        css = load_css('mouse').format(big_radius=scale * 30)
+        self.setStyleSheet(css)
+
+
+def load_css(name):
+    template_file = os.path.join(os.path.dirname(__file__), '%s.css.template' % name)
+    with open(template_file) as fobj:
+        css = fobj.read()
+    return css
