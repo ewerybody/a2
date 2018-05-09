485373f0f09028cff2e158e58235111c0b58b827
ewerybody <ewerybody+github@gmail.com>
2018-05-09 19:31:47 +0200

    new icon, main icons "less black"

--- ui/a2ctrl/base.py
+++ ui/a2ctrl/base.py
@@ -12,10 +12,19 @@ import a2core
 
 log = a2core.get_logger(__name__)
 ICO_PATH = None
+DEFAULT_ALPHA = 0.6
+LOW_ALPHA = 0.25
 
 
 class Ico(QtGui.QIcon):
     def __init__(self, ico_name, px=512, alpha=None):
+        """
+        :param str ico_name: Name of the icon. If present in icon library the path is
+            build there. Otherwise ico_name has to be a path to the image file.
+        :param int px: Prerender size of the icon image. Lower values than 512 might
+            cause artifacts but might be more memory and loading speed friendly.
+        :param float alpha: 0.0 to 1.0 transparency value for the icon image.
+        """
         super(Ico, self).__init__()
 
         self.px = px
@@ -82,11 +91,15 @@ class Ico(QtGui.QIcon):
 
     @property
     def tinted(self):
-        if self._tinted is not None:
-            return self._tinted
+        if self._tinted is None:
+            self._tinted = Ico(self.path, self.px, alpha=LOW_ALPHA)
 
-        self._tinted = Ico(self.path, self.px, alpha=0.3)
         return self._tinted
+
+
+class LibIco(Ico):
+    def __init__(self, name):
+        super(LibIco, self).__init__(name, 512, DEFAULT_ALPHA)
 
 
 class Icons(object):
@@ -110,38 +123,39 @@ class Icons(object):
         self.a2close = Ico('a2x')
         self.a2reload = Ico('a2reload')
         self.a2help = Ico('a2help')
-
         self.autohotkey = Ico('autohotkey')
-        self.button = Ico('button')
-        self.check = Ico('check')
-        self.check_circle = Ico('check_circle')
-        self.code = Ico('code')
-        self.copy = Ico('copy')
-        self.combo = Ico('combo')
-        self.clear = Ico('clear')
-        self.cloud_download = Ico('cloud_download')
-        self.cut = Ico('cut')
-        self.delete = Ico('delete')
-        self.down = Ico('down')
-        self.down_align = Ico('down_align')
-        self.down_circle = Ico('down_circle')
-        self.edit = Ico('edit')
-        self.error = Ico('error')
-        self.file_download = Ico('file_download')
-        self.folder = Ico('folder')
-        self.folder2 = Ico('folder2')
-        self.folder_add = Ico('folder_add')
-        self.group = Ico('group')
         self.github = Ico('github')
-        self.help = Ico('help')
-        self.hotkey = Ico('keyboard')
-        self.label = Ico('label')
-        self.label_plus = Ico('label_plus')
-        self.number = Ico('number')
-        self.paste = Ico('paste')
-        self.text = Ico('text')
-        self.reload = Ico('reload')
-        self.rollback = Ico('rollback')
-        self.string = Ico('string')
-        self.up = Ico('up')
-        self.up_align = Ico('up_align')
+
+        self.button = LibIco('button')
+        self.check = LibIco('check')
+        self.check_circle = LibIco('check_circle')
+        self.code = LibIco('code')
+        self.copy = LibIco('copy')
+        self.combo = LibIco('combo')
+        self.clear = LibIco('clear')
+        self.cloud_download = LibIco('cloud_download')
+        self.cut = LibIco('cut')
+        self.delete = LibIco('delete')
+        self.down = LibIco('down')
+        self.down_align = LibIco('down_align')
+        self.down_circle = LibIco('down_circle')
+        self.edit = LibIco('edit')
+        self.error = LibIco('error')
+        self.file_download = LibIco('file_download')
+        self.folder = LibIco('folder')
+        self.folder2 = LibIco('folder2')
+        self.folder_add = LibIco('folder_add')
+        self.group = LibIco('group')
+        self.help = LibIco('help')
+        self.hotkey = LibIco('keyboard')
+        self.label = LibIco('label')
+        self.label_plus = LibIco('label_plus')
+        self.more = LibIco('more')
+        self.number = LibIco('number')
+        self.paste = LibIco('paste')
+        self.text = LibIco('text')
+        self.reload = LibIco('reload')
+        self.rollback = LibIco('rollback')
+        self.string = LibIco('string')
+        self.up = LibIco('up')
+        self.up_align = LibIco('up_align')
