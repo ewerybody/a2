f2142f8f2864cfa751cca2e68928e9c7e7124134
ewerybody <ewerybody+github@gmail.com>
2018-05-15 21:52:14 +0200

    scope widget added picker icon

--- ui/a2widget/a2hotkey/scope_widget.py
+++ ui/a2widget/a2hotkey/scope_widget.py
@@ -25,54 +25,29 @@ class ScopeWidget(QtGui.QWidget):
         self._cfg = config
         self._line_height_set = False
 
-        # update check done in parent widget for proper order
-        self.ui = scope_widget_ui.Ui_ScopeWidget()
-        self.ui.setupUi(self)
-
-        icons = a2ctrl.Icons.inst()
-        self.ui.scope_add.clicked.connect(self.add_scope)
-        self.ui.scope_add.setIcon(icons.list_add)
-        self.ui.scope_delete.clicked.connect(self.delete_scope)
-        self.ui.scope_delete.setIcon(icons.delete)
-        self.ui.scope_pick.clicked.connect(self.pick_scope_info)
-        self.ui.scope_pick.setIcon(icons.locate)
-
-        self.ui.cfg_scope.itemSelectionChanged.connect(self.on_selection_change)
-
-        self.ui.cfg_scope.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
-        self.ui.cfg_scope.customContextMenuRequested.connect(self.build_list_context_menu)
-        self.context_menu = QtGui.QMenu(self)
-
-        self.context_menu.addAction(icons.delete, 'Delete scope item', self.delete_scope)
-
-        shortcut = QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Delete),
-                                   self.ui.cfg_scope, self.delete_scope)
-        shortcut.setContext(QtCore.Qt.WidgetShortcut)
-
+        self._setup_ui()
         self.get_scope_data()
         self.set_config()
 
-        self._scope_mode_changed.connect(self.on_scope_mode_change)
-
-        self.scope_ctrls = OrderedDict()
-        for name, ctrl in zip(SCOPE_ITEMS, [self.ui.scope_title, self.ui.scope_class,
-                                            self.ui.scope_exe]):
-            self.scope_ctrls[name] = ctrl
-
-        for i, ctrl in enumerate(self.scope_ctrls.values()):
-            ctrl.textChanged.connect(self.on_text_change)
-            ctrl.menu_about_to_show.connect(partial(self.build_button_field_menu, i))
-
     def on_text_change(self):
-        texts = [self.ui.scope_title.text()]
-        winclass = self.ui.scope_class.text()
-        if winclass:
-            texts.append('ahk_class ' + winclass)
-        winexe = self.ui.scope_exe.text()
-        if winexe:
-            texts.append('ahk_exe ' + winexe)
-        self.scope_string = ' '.join(texts).strip()
-        self.ui.scope_string.setText(self.scope_string)
+        scope_string = self.get_scope_string(
+            self.ui.scope_title.text(), self.ui.scope_class.text(), self.ui.scope_exe.text())
+        self.set_scope_string(scope_string)
+
+    def set_scope_string(self, scope_string):
+        selected_idxs = self.ui.cfg_scope.selectedIndexes()
+        if len(selected_idxs) == 1:
+            item = self.ui.cfg_scope.itemFromIndex(selected_idxs[0])
+            item.setText(scope_string)
+
+    def get_scope_string(self, title, class_name, process):
+        items = [title]
+        if class_name:
+            items.append('ahk_class ' + class_name)
+        if process:
+            items.append('ahk_exe ' + process)
+        scope_string = ' '.join(items).strip()
+        return scope_string
 
     def set_config(self, config_dict=None):
         if config_dict is not None:
@@ -84,10 +59,18 @@ class ScopeWidget(QtGui.QWidget):
         a2ctrl.connect.control_list([self.ui.scopeMode_1, self.ui.scopeMode_2],
                                     self._cfg, self._scope_mode_changed)
 
-    def on_selection_change(self, khasd=None):
-        print('khasd: %s' % khasd)
+    def on_selection_change(self):
         items = a2ctrl.qlist.get_selected_as_text(self.ui.cfg_scope)
         print('items: %s' % items)
+        if len(items) == 1:
+            scope_string = items[0]
+            for typ, ctrl in [('ahk_exe', self.ui.scope_exe),
+                              ('ahk_class', self.ui.scope_class)]:
+                found = scope_string.find(typ)
+                if found != -1:
+                    ctrl.setText(scope_string[found + len(typ):].strip())
+                    scope_string = scope_string[:found]
+            self.ui.scope_title.setText(scope_string.strip())
 
     def on_scope_mode_change(self):
         scope_mode = self._cfg.get('scopeMode')
@@ -175,14 +158,30 @@ class ScopeWidget(QtGui.QWidget):
         sender = self.sender()
         self.scope_ctrls[sender.data()].setText(sender.text())
 
+    def build_all_scopes_menu(self):
+        self.scopes_submenu.clear()
+
+        import a2runtime
+        collector = a2runtime.collect_includes(a2runtime.IncludeType.hotkeys)
+        scopes = list(collector.hotkeys.hotkeys_scope_incl.keys())
+        scopes.extend(collector.hotkeys.hotkeys_scope_excl.keys())
+
+        for scope in sorted(scopes, key=lambda s: s.lower()):
+            self.scopes_submenu.addAction(scope, self._add_scope_from_action)
+
+    def _add_scope_from_action(self):
+        scope_string = self.sender().text()
+        self.add_scope(scope_string)
+
     def pick_scope_info(self):
         #a2 = a2core.A2Obj.inst()
         #text = a2ahk.call_lib_cmd('pick_scope_nfo', cwd=a2.paths.a2)
-        text = a2ahk.call_lib_cmd('pick_scope_nfo')
+        scope_nfo_string = a2ahk.call_lib_cmd('pick_scope_nfo')
         try:
-            title, class_name, process = text.split('\n')
+            title, class_name, process = scope_nfo_string.split('\\n')
+            for name, value in zip(SCOPE_ITEMS, [title, class_name, process]):
+                self.scope_ctrls[name].setText(value)
 
-            print('text: %s' % text)
         except ValueError:
             pass
 
@@ -203,16 +202,53 @@ class ScopeWidget(QtGui.QWidget):
                 if this_value:
                     self.system_scope_data[SCOPE_ITEMS[j]].add(this_value)
 
-    def build_all_scopes_menu(self):
-        self.scopes_submenu.clear()
+    def _setup_ui(self):
+        # update check done in parent widget for proper order
+        self.ui = scope_widget_ui.Ui_ScopeWidget()
+        self.ui.setupUi(self)
 
-        import a2runtime
-        collector = a2runtime.collect_includes(a2runtime.IncludeType.hotkeys)
-        scopes = list(collector.hotkeys.hotkeys_scope_incl.keys())
-        scopes.extend(collector.hotkeys.hotkeys_scope_excl.keys())
+        self.scope_ctrls = OrderedDict()
+        for name, ctrl in zip(SCOPE_ITEMS, [self.ui.scope_title,
+                              self.ui.scope_class, self.ui.scope_exe]):
+            self.scope_ctrls[name] = ctrl
 
-        for scope in sorted(scopes, key=lambda s: s.lower()):
-            self.scopes_submenu.addAction(scope, self.set_scope_string)
+        icons = a2ctrl.Icons.inst()
+        self.ui.scope_add.clicked.connect(self.add_scope)
+        self.ui.scope_add.setIcon(icons.list_add)
+        self.ui.scope_delete.clicked.connect(self.delete_scope)
+        self.ui.scope_delete.setIcon(icons.delete)
+        self.ui.scope_pick.clicked.connect(self.pick_scope_info)
+        self.ui.scope_pick.setIcon(icons.locate)
+
+        self.ui.cfg_scope.itemSelectionChanged.connect(self.on_selection_change)
+
+        self.ui.cfg_scope.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.ui.cfg_scope.customContextMenuRequested.connect(self.build_list_context_menu)
+        self.context_menu = QtGui.QMenu(self)
+        self.context_menu.addAction(icons.delete, 'Delete scope item', self.delete_scope)
+
+        shortcut = QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Delete),
+                                   self.ui.cfg_scope, self.delete_scope)
+        shortcut.setContext(QtCore.Qt.WidgetShortcut)
+        self._scope_mode_changed.connect(self.on_scope_mode_change)
+
+        for i, ctrl in enumerate(self.scope_ctrls.values()):
+            ctrl.textChanged.connect(self.on_text_change)
+            ctrl.menu_about_to_show.connect(partial(self.build_button_field_menu, i))
+
+        self.add_menu = QtGui.QMenu(self)
+#        submenu = QtGui.QMenu(self.add_menu)
+#        submenu.setTitle('all in use...')
+#        for scope in sorted(self.a2.get_used_scopes(), key=lambda s: s.lower()):
+#            action = QtGui.QAction(scope, submenu, triggered=partial(self.set_scope_string, scope))
+#            submenu.addAction(action)
+#        menu.addMenu(submenu)
+
+        self.scopes_submenu = self.add_menu.addMenu('All Scopes in use')
+        self.scopes_submenu.aboutToShow.connect(self.build_all_scopes_menu)
+#        self.ui.scope_string.menu.addSeparator()
+#        for title in self.help_map:
+#            self.ui.scope_string.add_action(title, self.surf_to_help)
 
 
 if __name__ == '__main__':
