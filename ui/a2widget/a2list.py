b473a3c1b60d30df2f6b90d8f363399b989e20fd
ewerybody <ewerybody+github@gmail.com>
2018-06-07 20:35:12 +0200

    finally a new list widget!

---
+++ ui/a2widget/a2list.py
@@ -0,0 +1,231 @@
+from PySide import QtGui, QtCore
+
+
+class A2List(QtGui.QListWidget):
+    """
+    A simple list widget.
+    """
+    names_selected = QtCore.Signal(list)
+    items_selected = QtCore.Signal(list)
+    single_name_selected = QtCore.Signal(str)
+    single_item_selected = QtCore.Signal(QtGui.QListWidgetItem)
+    selection_cleared = QtCore.Signal()
+
+    name_double_clicked = QtCore.Signal(str)
+    item_double_clicked = QtCore.Signal(QtGui.QListWidgetItem)
+    changed = QtCore.Signal()
+    item_removed = QtCore.Signal()
+    context_menu_requested = QtCore.Signal(QtCore.QPoint)
+
+    def __init__(self, parent=None, names=None):
+        """
+
+        """
+        super(A2List, self).__init__(parent)
+        self._unique = True
+
+        if names:
+            self.blockSignals(True)
+            self.add(names)
+            self.blockSignals(False)
+
+        self.itemSelectionChanged.connect(self._on_selection_change)
+
+        shortcut = QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Delete),
+                                   self, self.remove_selected)
+        shortcut.setContext(QtCore.Qt.WidgetShortcut)
+        self.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.context_menu_requested.emit)
+
+    def add(self, names):
+        current = self.get_names_lower()
+        addded_something = False
+
+        for NAME in ensure_list(names):
+            name = NAME.lower()
+            if self._unique:
+                if name in current:
+                    continue
+                current.append(name)
+
+            item = QtGui.QListWidgetItem(NAME)
+            self.addItem(item)
+
+            addded_something = True
+
+        if addded_something:
+            item.setSelected(True)
+            self.changed.emit()
+
+    def set_names(self, names):
+        self.clear()
+        self.add(names)
+
+    def iter_items(self):
+        for i in range(self.count()):
+            yield self.item(i)
+
+    def get_items(self):
+        return [self.item(i) for i in range(self.count())]
+
+    def get_items_from_names(self, names):
+        items = []
+        names = [n.lower() for n in ensure_list(names)]
+        for item in self.iter_items():
+            if item.text().lower() in names:
+                items.append(item)
+        return items
+
+    def get_names(self):
+        return [self.item(i).text() for i in range(self.count())]
+
+    def get_names_lower(self):
+        return [self.item(i).text().lower() for i in range(self.count())]
+
+    def contains(self, names):
+        current = self.get_names_lower()
+        for name in ensure_list(names):
+            if name.lower() not in current:
+                return False
+        return True
+
+    @property
+    def unique(self):
+        return self._unique
+
+    def set_unique(self, state):
+        self._unique = state
+
+    def get_selected_names(self):
+        return [i.text() for i in self.selectedItems()]
+
+    def get_selected_items(self):
+        return self.selectedItems()
+
+    def set_item_name(self, new_name, item=None):
+        if item is None:
+            selected = self.get_selected_items()
+            if len(selected) != 1:
+                raise RuntimeError('Cannot set name! No single selection '
+                                   'and No item given!')
+            item = selected[0]
+
+        item.setText(new_name)
+
+    def select_names(self, names=None):
+        items = self.get_items_from_names(names)
+        self.select_items(items)
+
+    def select_items(self, items):
+        """
+        WTF!?: there is an error when checking if a QListWidgetItem is  in a list
+        of QListWidgetItems via "item in item_list":
+            NotImplementedError: operator not implemented.
+        This uses a workaround getting the objects id()s.
+        """
+        if not items:
+            self.clear_selection()
+            return
+
+        item_ids = [id(i) for i in items]
+
+        last_item = None
+        for item in self.iter_items():
+            if id(item) in item_ids:
+                item.setSelected(True)
+                last_item = item
+            else:
+                item.setSelected(False)
+
+        if last_item is not None:
+            self.setCurrentItem(last_item)
+
+    def clear_selection(self):
+        [i.setSelected(False) for i in self.selectedItems()]
+
+    def _on_selection_change(self):
+        items = self.get_selected_items()
+        names = self.get_selected_names()
+        self.items_selected.emit(items)
+        self.names_selected.emit(names)
+        if len(items) == 1:
+            self.single_item_selected.emit(items[0])
+            self.single_name_selected.emit(names[0])
+        if len(items) == 0:
+            self.selection_cleared.emit()
+
+    def remove_selected(self):
+        self.remove_items(self.selectedItems())
+
+    def remove_names(self, names):
+        items = self.get_items_from_names(names)
+        self.remove_items(items)
+
+    def remove_items(self, items):
+        for item in items:
+            idx = self.indexFromItem(item)
+            self.takeItem(idx.row())
+        self.changed.emit()
+
+
+class A2ListCompact(A2List):
+    def __init__(self, parent=None, names=None):
+        super(A2ListCompact, self).__init__(parent, names)
+        self._shown = False
+        self.changed.connect(self._set_list_line_height)
+        self._max_lines_visible = 5
+        self._list_line_height = None
+        self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+
+    @property
+    def max_lines_visible(self):
+        return self._max_lines_visible
+
+    def set_max_lines_visible(self, num_lines):
+        self._max_lines_visible = num_lines
+        self._set_list_line_height()
+
+    def showEvent(self, *args, **kwargs):
+        self._shown = True
+        self._set_list_line_height()
+        return QtGui.QListWidget.showEvent(self, *args, **kwargs)
+
+    def _init_line_height(self):
+        # figure out list_widget row height
+        if self._list_line_height is None:
+            item = self.item(0)
+            dummy_create = item is None
+            if dummy_create:
+                item = QtGui.QListWidgetItem('')
+                self.addItem(item)
+            self._list_line_height = self.visualItemRect(item).height()
+            if dummy_create:
+                self.takeItem(0)
+        return self._list_line_height
+
+    def _set_list_line_height(self):
+        if not self._shown:
+            return
+
+        self._init_line_height()
+        num_items = self.count()
+        h = self._list_line_height * min(max(num_items, 1), self._max_lines_visible)
+        h += self._list_line_height / 3
+        self.setMinimumHeight(h)
+        self.setMaximumHeight(h)
+        self._line_height_set = True
+
+
+def ensure_list(strings):
+    if not isinstance(strings, (str, list, tuple, set)):
+        raise TypeError('Please provide a string, list, tuple or set')
+
+    if isinstance(strings, str):
+        strings = [strings]
+
+    return strings
+
+
+if __name__ == '__main__':
+    import a2widget.demo.a2list_demo
+    a2widget.demo.a2list_demo.show()
---
+++ ui/a2widget/demo/a2list_demo.py
@@ -0,0 +1,63 @@
+import uuid
+from PySide import QtGui
+from a2widget import A2List, A2ListCompact
+
+
+class Demo(QtGui.QMainWindow):
+    def __init__(self):
+        super(Demo, self).__init__()
+        w = QtGui.QWidget(self)
+        self.setCentralWidget(w)
+        lyt = QtGui.QVBoxLayout(w)
+        w.setLayout(lyt)
+
+        lyt.addWidget(QtGui.QLabel('a simple list:'))
+        list_items = 'mango banana apple kiwi apple strawberry'.split()
+        self.widget1 = A2List(self, list_items)
+        lyt.addWidget(self.widget1)
+
+        self.widget1.items_selected.connect(self.on_things_selection)
+        self.widget1.names_selected.connect(self.on_things_selection)
+        self.widget1.single_item_selected.connect(self.on_thing_selection)
+        self.widget1.single_name_selected.connect(self.on_thing_selection)
+        self.widget1.changed.connect(self.changed1)
+
+        lyt.addWidget(QtGui.QLabel('a height adjusted list:'))
+        self.widget2 = A2ListCompact(self)
+        lyt.addWidget(self.widget2)
+
+        self.widget2.items_selected.connect(self.on_things_selection)
+        self.widget2.names_selected.connect(self.on_things_selection)
+        self.widget2.single_item_selected.connect(self.on_thing_selection)
+        self.widget2.single_name_selected.connect(self.on_thing_selection)
+        self.widget2.changed.connect(self.changed2)
+
+        button = QtGui.QPushButton('add random')
+        button.clicked.connect(self.bla)
+        lyt.addWidget(button)
+
+    def bla(self):
+        self.widget2.add(str(uuid.uuid4()))
+
+    def on_things_selection(self, things):
+        print('things: %s' % things)
+
+    def on_thing_selection(self, thing):
+        print('single thing: %s' % thing)
+
+    def changed1(self):
+        print('list 1 changed!')
+
+    def changed2(self):
+        print('list 2 changed!')
+
+
+def show():
+    app = QtGui.QApplication([])
+    win = Demo()
+    win.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    show()
