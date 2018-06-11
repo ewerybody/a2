7c411be257cb7283b1345f46c3bbeaaba0e8fc79
ewerybody <ewerybody+github@gmail.com>
2018-06-11 16:21:20 +0200

    cleaned up scope dialog, added validation

--- ui/a2widget/a2hotkey/scope_dialog.py
+++ ui/a2widget/a2hotkey/scope_dialog.py
@@ -1,13 +1,9 @@
-import a2ahk
 import a2core
 import a2ctrl
-import a2util
-from collections import OrderedDict
 from PySide import QtGui, QtCore
 
 from . import scope_dialog_ui
 from .hotkey_widget import Vars
-from functools import partial
 
 
 log = a2core.get_logger(__name__)
@@ -24,128 +20,29 @@ class ScopeDialog(QtGui.QDialog):
         self.ui.setupUi(self)
         self.setWindowTitle('Setup Scope')
         self.ui.scope_widget.set_config(config)
-
+        self.ui.scope_widget.changed.connect(self.check)
         self.ui.display_only_label.setVisible(not config.get(Vars.scope_change, False))
-        # self.system_scope_data = {}
-        # self.get_scope_data()
-        #
-        # self.a2 = a2core.A2Obj.inst()
-        # self.help_map = OrderedDict()
-        # self.help_map['Help on Scope Setup'] = self.a2.urls.helpScopes
-        # self.help_map['Help on AHK WinActive'] = self.a2.urls.ahkWinActive
-        # self.help_map['Help on AHK WinTitle'] = self.a2.urls.ahkWinTitle
 
-        self.setup_ui()
-        # self.set_scope_string()
-
-    def setup_ui(self):
         self.ui.a2ok_button.clicked.connect(self.ok)
         self.ui.a2cancel_button.clicked.connect(self.reject)
 
-        # pos = self.pos()
-        # cursor_pos = QtGui.QCursor.pos()
-        # pos.setX(cursor_pos.x() - (self.width() / 2))
-        # pos.setY(cursor_pos.y() - (self.height() / 2))
-        # self.move(pos)
-        # self.ui.scope_title.setFocus()
-        #
-        # self.scope_ctrls = OrderedDict()
-        # for name, ctrl in zip(SCOPE_ITEMS, [self.ui.scope_title, self.ui.scope_class,
-        #                                     self.ui.scope_exe]):
-        #     self.scope_ctrls[name] = ctrl
-        #
-        # for i, ctrl in enumerate(self.scope_ctrls.values()):
-        #     ctrl.textChanged.connect(self.text_change)
-        #     ctrl.menu_about_to_show.connect(partial(self.build_button_menu, i))
-#        menu = QtGui.QMenu(self)
-#        submenu = QtGui.QMenu(menu)
-#        submenu.setTitle('all in use...')
-#        for scope in sorted(self.a2.get_used_scopes(), key=lambda s: s.lower()):
-#            action = QtGui.QAction(scope, submenu, triggered=partial(self.set_scope_string, scope))
-#            submenu.addAction(action)
-#        menu.addMenu(submenu)
+    def showEvent(self, *args, **kwargs):
+        self.ui.scope_widget.setMinimumWidth(self.minimumSizeHint().height() * 3)
+        return QtGui.QDialog.showEvent(self, *args, **kwargs)
 
-#        self.scopes_submenu = self.ui.scope_string.menu.addMenu('All Scopes in use')
-#        self.scopes_submenu.aboutToShow.connect(self.build_all_scopes_menu)
-        # self.ui.scope_string.menu.addSeparator()
-#        for title in self.help_map:
-#            self.ui.scope_string.add_action(title, self.surf_to_help)
-
-    def build_all_scopes_menu(self):
-        self.scopes_submenu.clear()
-
-        import a2runtime
-        collector = a2runtime.collect_includes(a2runtime.IncludeType.hotkeys)
-        scopes = list(collector.hotkeys.hotkeys_scope_incl.keys())
-        scopes.extend(collector.hotkeys.hotkeys_scope_excl.keys())
-
-        for scope in sorted(scopes, key=lambda s: s.lower()):
-            self.scopes_submenu.addAction(scope, self.set_scope_string)
-
-    def build_button_menu(self, index, menu):
-        menu.clear()
-        name = SCOPE_ITEMS[index]
-        # usedmenu = menu.addMenu('%s in use' % name.title())
-        submenu = menu.addMenu('Available %s' % name.title())
-        for item in sorted(self.system_scope_data[name], key=lambda s: s.lower()):
-            # action = QtGui.QAction(item, submenu, triggered=partial(self.set_scope, i, item))
-            action = submenu.addAction(item, self.set_menu_item)
-            action.setData(name)
-
-    def set_menu_item(self):
-        sender = self.sender()
-        self.scope_ctrls[sender.data()].setText(sender.text())
-
-    def surf_to_help(self):
-        url = self.help_map[self.sender().text()]
-        a2util.surf_to(url)
-
-    def set_scope_config(self, config=None):
-        if config is None:
-            scope_string = self.sender().text()
-        self.ui.scope_string.setText(scope_string)
-
-        # from given text fill the line edits
-        if scope_string:
-            for typ, ctrl in [('ahk_exe', self.ui.scope_exe),
-                              ('ahk_class', self.ui.scope_class)]:
-                found = scope_string.find(typ)
-                if found != -1:
-                    ctrl.setText(scope_string[found + len(typ):].strip())
-                    scope_string = scope_string[:found]
-            self.ui.scope_title.setText(scope_string.strip())
-
-    def text_change(self):
-        texts = [self.ui.scope_title.text()]
-        winclass = self.ui.scope_class.text()
-        if winclass:
-            texts.append('ahk_class ' + winclass)
-        winexe = self.ui.scope_exe.text()
-        if winexe:
-            texts.append('ahk_exe ' + winexe)
-        self.scope_string = ' '.join(texts).strip()
-        self.ui.scope_string.setText(self.scope_string)
-
-    def set_scope(self, index, text):
-        ctrls = [self.ui.scope_string, self.ui.scope_title, self.ui.scope_class, self.ui.scope_exe]
-        ctrls[index].setText(text)
-
-    def get_scope_data(self):
-        # call AHK script to get all window classes, titles and executables
-        scope_nfo = a2ahk.call_lib_cmd('get_scope_nfo')
-        scope_nfo = scope_nfo.split('\\n')
-        if not scope_nfo:
-            log.error('Error getting scope_nfo!! scope_nfo: %s' % scope_nfo)
-            return
-
-        self.system_scope_data = dict([(n, set()) for n in SCOPE_ITEMS])
-        num_items = len(scope_nfo)
-        num_items -= num_items % 3
-        for i in range(0, num_items, 3):
-            for j in range(3):
-                this_value = scope_nfo[i + j]
-                if this_value:
-                    self.system_scope_data[SCOPE_ITEMS[j]].add(this_value)
+    def check(self):
+        cfg = self.ui.scope_widget.get_config()
+        if not cfg[Vars.scope]:
+            if cfg[Vars.scope_mode] == 1:
+                self.ui.a2ok_button.setEnabled(False)
+                self.ui.a2ok_button.setText('Include "nothing" Deativates the scope!')
+                return
+            elif cfg[Vars.scope_mode] == 2:
+                self.ui.a2ok_button.setEnabled(False)
+                self.ui.a2ok_button.setText('Exclude "nothing" makes it global!')
+                return
+        self.ui.a2ok_button.setText('OK')
+        self.ui.a2ok_button.setEnabled(True)
 
     def ok(self):
         cfg = self.ui.scope_widget.get_config()
