0d94034375b370353c8db1f2c7cd4f37284a1633
Eric Werner <ewerybody+github@gmail.com>
2018-06-17 20:55:23 +0200

    more desc typos ...

--- ui/siding/addons/base.py
+++ ui/siding/addons/base.py
@@ -51,8 +51,8 @@ def action(text=None, icon=None, status_tip=None, tool_tip=None,
     ===========  ============
     text         The text to display for the action. By default, this is set to the function name after underscores have been converted to spaces and it has been capitalized.
     icon         The icon, if any, to display for the action. See :meth:`PySide.QtGui.QAction.icon() <PySide.QtGui.QAction.icon>`
-    status_tip   The status tip for the action. See :meth:`PySide.QtGui.QAction.statusTip <PySide.QtGui.PySide.QtGui.QAction.statusTip>`
-    tool_tip     The tool tip for the action. See :meth:`PySide.QtGui.QAction.toolTip  <PySide.QtGui.PySide.QtGui.QAction.toolTip>`
+    status_tip   The status tip for the action. See :meth:`PySide.QtGui.QAction.statusTip <PySide.QtGui.QAction.statusTip>`
+    tool_tip     The tool tip for the action. See :meth:`PySide.QtGui.QAction.toolTip  <PySide.QtGui.QAction.toolTip>`
     whats_this   The 'What's This?' text for the action. See :meth:`PySide.QtGui.QAction.whatsThis  <PySide.QtGui.QAction.whatsThis>`
     default      If this is True, this action will be made the default and, if an add-on is double clicked, will be executed.
     checkable    Whether or not the action's state can be toggled.
