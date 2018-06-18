822491331f69600bf608f58ad737fc614b1d356c
ewerybody <ewerybody+github@gmail.com>
2018-06-18 14:58:40 +0200

    added 'common' module for hotkeys stuff

---
+++ ui/a2widget/a2hotkey/hotkey_common.py
@@ -0,0 +1,9 @@
+class Vars(object):
+    scope = 'scope'
+    scope_mode = 'scopeMode'
+    scope_change = 'scopeChange'
+
+    function_mode = 'functionMode'
+    function_code = 'functionCode'
+    function_url = 'functionURL'
+    function_send = 'functionSend'
