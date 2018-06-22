6fb5aa34edc2b98499f7c06a84e09aafb803bdbf
ewerybody <ewerybody+github@gmail.com>
2018-06-22 16:09:53 +0200

    after linting ... some nicer code

--- ui/a2element/pathlist.py
+++ ui/a2element/pathlist.py
@@ -1,21 +1,12 @@
 # -*- coding: utf-8 -*-
-"""
-Some element description ...
-
-@created: 2017 11 1
-@author: Eric Werner
-"""
-import a2ctrl
 from PySide2 import QtCore, QtWidgets
-from a2element import pathlist_edit_ui, DrawCtrl, EditCtrl
+
+import a2ctrl
 from a2widget import A2PathField
+from a2element import pathlist_edit_ui, DrawCtrl, EditCtrl
 
 
 class Draw(QtWidgets.QGroupBox, DrawCtrl):
-    """
-    The frontend widget visible to the user with options
-    to change the default behavior of the element.
-    """
     def __init__(self, main, cfg, mod):
         super(Draw, self).__init__(main)
         DrawCtrl.__init__(self, main, cfg, mod, _init_ctrl=False)
@@ -29,6 +20,7 @@ class Draw(QtWidgets.QGroupBox, DrawCtrl):
             self.add_path(path)
 
     def add_path(self, path=''):
+        """Adds a path to the list."""
         path_widget = PathEntry(self, self.a2_group_layout.count(),
                                 self.cfg.get('browse_type', 0), path)
         path_widget.changed.connect(self.check)
@@ -40,26 +32,26 @@ class Draw(QtWidgets.QGroupBox, DrawCtrl):
     def _path_removed(self, del_index):
         del_widget = self.path_widgets.pop(del_index)
         # fix index labels
-        for i, widget in enumerate(self.path_widgets):
-            widget.index = i
+        for index, widget in enumerate(self.path_widgets):
+            widget.index = index
 
         # avoid check when path was empty
         if del_widget.path:
             self.check()
 
-    def _make_path_widget(self, i, path=''):
+    def _make_path_widget(self, index, path=''):
         widget = QtWidgets.QWidget(self)
         layout = QtWidgets.QHBoxLayout(widget)
         layout.setContentsMargins(0, 0, 0, 0)
-        widget.label = QtWidgets.QLabel(str(i + 1))
+        widget.label = QtWidgets.QLabel(str(index + 1))
         layout.addWidget(widget.label)
         field = A2PathField(widget, path, writable=False)
         field.browse_type = self.cfg.get('browse_type', 0)
         field.changed.connect(self.check)
         layout.addWidget(field)
         button = QtWidgets.QToolButton(self)
-        if i:
-            button.path_index = i
+        if index:
+            button.path_index = index
             button.setIcon(a2ctrl.Icons.inst().clear)
             button.clicked.connect(self.remove_path)
         else:
@@ -69,10 +61,6 @@ class Draw(QtWidgets.QGroupBox, DrawCtrl):
         self.a2_group_layout.addWidget(widget)
         return field
 
-    def _check_indices(self):
-        for i, widget in enumerate(self.path_widgets):
-            print('widget.label.text() %i: %s' % (i, widget.label.text()))
-
     def check(self, *_args):
         path_list = [w.path for w in self.path_widgets if w.path]
         self.set_user_value(path_list)
@@ -80,10 +68,6 @@ class Draw(QtWidgets.QGroupBox, DrawCtrl):
 
 
 class Edit(EditCtrl):
-    """
-    The background widget that sets up how the user can edit the element,
-    visible when editing the module.
-    """
     def __init__(self, cfg, main, parent_cfg):
         super(Edit, self).__init__(cfg, main, parent_cfg, add_layout=False)
 
@@ -103,6 +87,7 @@ class Edit(EditCtrl):
 
 
 class PathEntry(QtWidgets.QWidget):
+    """A single line widget for each entry in the ui"""
     changed = QtCore.Signal(str)
     delete_me = QtCore.Signal(int)
     add_path = QtCore.Signal()
@@ -132,18 +117,34 @@ class PathEntry(QtWidgets.QWidget):
 
     @property
     def path(self):
+        """
+        Holds the path of the widget
+        :rtype: str
+        """
         return self._field.value
 
     @property
     def index(self):
+        """
+        Holds the index of the widget
+        :rtype: int
+        """
         return self._index
 
     @index.setter
     def index(self, value):
+        """
+        Sets the index number and label.
+        Does NOT actually move the widget!
+        :param int value: Index to set.
+        """
         self._index = value
         self._label.setText(str(value + 1))
 
     def delete(self):
+        """
+        Tells Qt and the parent widget to delete this one.
+        """
         self.deleteLater()
         self.delete_me.emit(self.index)
 
@@ -151,33 +152,8 @@ class PathEntry(QtWidgets.QWidget):
         return '<PathEntry %i "%s" at %s>' % (self.index, self.path, id(self))
 
 
-def get_settings(module_key, cfg, db_dict, user_cfg):
-    """
-    Called by the module on "change" to get an elements data thats
-    eventually written into the runtime includes.
-
-    Passed into is all you might need:
-    :param str module_key: "module_source_name|module_name" combo used to identify module in db
-    :param dict cfg: Standard element configuration dictionary.
-    :param dict db_dict: Dictionary that's used to write the include data with "hotkeys", "variables" and "includes" keys
-    :param dict user_cfg: This elements user edits saved in the db
-
-    To make changes to the:
-    * "variables" - a simple key, value dictionary in db_dict
-
-    Get the current value via get_cfg_value() given the default cfg and user_cfg.
-    If value name is found it takes the value from there, otherwise from cfg or given default.
-
-        value = a2ctrl.get_cfg_value(cfg, user_cfg, typ=bool, default=False)
-
-    write the key and value to the "variables" dict:
-
-        db_dict['variables'][cfg['name']] = value
-
-    * "hotkeys" - a dictionary with scope identifiers
-
-    * "includes" - a simple list with ahk script paths
-    """
+def get_settings(_module_key, cfg, db_dict, user_cfg):
+    """default get_settings"""
     db_dict.setdefault('variables', {})
     value = a2ctrl.get_cfg_value(cfg, user_cfg, typ=list, default=[])
     db_dict['variables'][cfg['name']] = value
