417c2fdd35f4a7dbd44d9d1962b8f99336457926
Eric Werner <ewerybody+github@gmail.com>
2018-07-03 23:12:41 +0200

    hotkey widget may display multiple already wip #64

--- ui/a2widget/a2hotkey/hotkey_widget.py
+++ ui/a2widget/a2hotkey/hotkey_widget.py
@@ -33,42 +33,56 @@ class A2Hotkey(QtWidgets.QWidget):
         self._edit_mode = False
         self.scope_data = scope_data
 
+        self._vlayout = QtWidgets.QVBoxLayout(self)
+        self._vlayout.setSpacing(0)
+        self._vlayout.setContentsMargins(0, 0, 0, 0)
         self._layout = QtWidgets.QHBoxLayout(self)
+        self._vlayout.addLayout(self._layout)
         self._layout.setContentsMargins(0, 0, 0, 0)
         self._layout.setSpacing(0)
-        self._hotkey_button = QtWidgets.QPushButton(self)
-        self._hotkey_button.setObjectName('A2HotkeyButton')
+        self._hotkey_buttons = [QtWidgets.QPushButton(self)]
+        self._hotkey_buttons[0].setObjectName('A2HotkeyButton')
         self._scope_button = QtWidgets.QPushButton(self)
         self._scope_button.setObjectName('A2HotkeyScope')
-        self._layout.addWidget(self._hotkey_button)
+        self._layout.addWidget(self._hotkey_buttons[0])
         self._layout.addWidget(self._scope_button)
         self._scope_button.clicked.connect(self.scope_clicked)
-        self._hotkey_button.clicked.connect(self.popup_dialog)
-        self.setText(key)
+        self._hotkey_buttons[0].clicked.connect(self.popup_dialog)
 
         self.dialog_styles = [HotKeyBoard]
         self.dialog_default = HotkeyDialog1
         self.dialog_styles.append(self.dialog_default)
-        # self.set_config(None)
 
     def set_config(self, config_dict):
+        print('set_config on: %s' % self.parent())
         self._cfg = config_dict or {}
         if not self.is_edit_mode and not self._cfg.get(Vars.key_change, True):
-            self._hotkey_button.setEnabled(False)
-            self._hotkey_button.setToolTip(HOTKEY_CANNOT_CHANGE)
+            for button in self._hotkey_buttons:
+                button.setEnabled(False)
+                button.setToolTip(HOTKEY_CANNOT_CHANGE)
         self.setup_scope_button()
+        self.set_key(self._cfg.get('key', ''))
 
-        self.key = self._cfg.get('key', '')
-        self.setText(self.key)
-
-    def setText(self, text):
-        self._hotkey_button.setText(text)
+    def setText(self, key):
+        if isinstance(key, list):
+            key = key[0]
+        self._hotkey_buttons[0].setText(key)
 
     def set_key(self, key):
         if key != self.key:
             self.key = key
-            self.setText(key)
-            self.hotkey_changed.emit(key)
+            keys = [key] if isinstance(key, str) else key
+            if len(keys) > len(self._hotkey_buttons):
+                for i in range(len(self._hotkey_buttons), len(keys)):
+                    button = QtWidgets.QPushButton(self)
+                    self._vlayout.addWidget(button)
+                    self._hotkey_buttons.append(button)
+                    print('adding index: %i' % i)
+
+            for i in range(len(keys)):
+                self._hotkey_buttons[i].setText(keys[i])
+
+            # self.hotkey_changed.emit(self.key)
 
     def popup_dialog(self):
         class_name = self.a2.db.get('hotkey_dialog_style')
