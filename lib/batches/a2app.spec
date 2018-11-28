022c3b31859fa0148dbcb7c7196d82c78ca2a939
Eric Werner <ewerybody+github@gmail.com>
2018-11-28 18:37:50 +0100

    fixed package build script #153

--- lib/batches/a2app.spec
+++ lib/batches/a2app.spec
@@ -5,22 +5,25 @@ block_cipher = None
 
 a = Analysis(['C:\\Users\\eric\\io\\code\\a2\\lib\\batches\\..\\..\\ui\\a2app.py'],
              pathex=['C:\\Users\\eric\\io\\code\\a2\\lib\\batches\\'],
-             binaries=None,
-             datas=None,
+             binaries=[],
+             datas=[],
              hiddenimports=[],
              hookspath=[],
              runtime_hooks=[],
              excludes=[],
              win_no_prefer_redirects=False,
              win_private_assemblies=False,
-             cipher=block_cipher)
+             cipher=block_cipher,
+             noarchive=False)
 pyz = PYZ(a.pure, a.zipped_data,
              cipher=block_cipher)
 exe = EXE(pyz,
           a.scripts,
+          [],
           exclude_binaries=True,
           name='a2app',
           debug=False,
+          bootloader_ignore_signals=False,
           strip=False,
           upx=False,
           console=False , icon='C:\\Users\\eric\\io\\code\\a2\\ui\\res\\a2.ico')
--- lib/batches/build_py_package.py
+++ lib/batches/build_py_package.py
@@ -6,12 +6,12 @@ Whow! Batch files are such a pain. Petter keep them as short as possible.
 import os
 import sys
 import shutil
-from os.path import join, dirname, abspath, isfile
+from os.path import join
 
 
 def main(package_name):
-    this_path = dirname(__file__)
-    a2path = abspath(join(this_path, '..', '..'))
+    this_path = os.path.dirname(__file__)
+    a2path = os.path.abspath(join(this_path, '..', '..'))
     uipath = join(a2path, 'ui')
     sys.path.append(uipath)
     import a2ahk
@@ -22,28 +22,35 @@ def main(package_name):
     os.rename(join(distpath, 'a2app'), distui)
     print('copying root files ...')
 
-    for item in os.listdir(a2path):
-        if item in ['a2_init.ahk', 'a2_settings.ahk']:
+    for item in os.scandir(a2path):
+        if item.name in ['a2_init.ahk', 'a2_settings.ahk']:
             continue
-        path = join(a2path, item)
-        if isfile(path):
-            if item.endswith('.ahk') or item.endswith('.exe') or item in ['LICENSE']:
-                shutil.copy2(path, distpath)
+
+        if item.is_file():
+            if item.name.endswith('.ahk') or item.name in ['LICENSE']:
+                shutil.copy2(item.path, distpath)
 
     print('copying lib files ...')
     distlib = join(distpath, 'lib')
     a2lib = join(a2path, 'lib')
     os.mkdir(distlib)
-    for item in os.listdir(a2lib):
-        if item == 'batches':
+    for item in os.scandir(a2lib):
+        base, ext = os.path.splitext(item.name)
+        if base in ['batches', '_source', 'a2ui', 'a2ui dev',
+                    'a2init_check', 'a2dev_find_py', 'a2init_check']:
+            continue
+        if base.startswith('_ '):
             continue
 
-        path = join(a2lib, item)
-        if isfile(path):
-            if item.endswith('.ahk'):
-                shutil.copy2(path, distlib)
+        if item.name == 'a2ui release.ahk':
+            shutil.copy2(item.path, join(distlib, 'a2ui.ahk'))
+            continue
+
+        if item.is_file():
+            if ext == '.ahk':
+                shutil.copy2(item.path, distlib)
         else:
-            shutil.copytree(path, join(distlib, item))
+            shutil.copytree(item.path, join(distlib, item.name))
 
     print('copying ui files ...')
     a2uipath = join(a2path, 'ui')
@@ -52,14 +59,27 @@ def main(package_name):
         return [f for f in items if f.endswith('.ui')] + ['__pycache__', 'demo', 'work']
 
     for folder in ['a2ctrl', 'a2widget', 'a2element', 'res', 'style']:
-        shutil.copytree(join(a2uipath, folder), join(distui, folder), ignore=ui_ignore)
+        shutil.copytree(join(a2uipath, folder),
+                        join(distui, folder), ignore=ui_ignore)
 
-    for folder in ['examples', 'docs', 'include', 'translations']:
-        shutil.rmtree(join(distui, 'PySide', folder), ignore_errors=True)
+    shutil.rmtree(join(distui, 'PySide', folder), ignore_errors=True)
+    for item in os.scandir(distui):
+        if item.name.startswith('libopenblas.') and item.name.endswith('.dll'):
+            print('removing libopenblas ...')
+            os.remove(item.path)
+            break
 
-#     settings_file = join(distlib, '_defaults', 'a2_settings.ahk')
-#     a2ahk.set_variable(settings_file, 'a2_ui_call', 'a2app.exe')
-#     a2ahk.set_variable(settings_file, 'a2_title', package_name)
+    numpy_path = join(distui, 'numpy')
+    if os.path.isdir(numpy_path):
+        print('removing numpy ...')
+        shutil.rmtree(numpy_path, ignore_errors=True)
+
+    github_link_name = 'a2 on github.com.URL'
+    shutil.copy(join(a2path, github_link_name),
+                join(distpath, github_link_name))
+
+    config_file = join(distlib, 'a2_config.ahk')
+    a2ahk.set_variable(config_file, 'a2_title', package_name)
 
 
 if __name__ == '__main__':
