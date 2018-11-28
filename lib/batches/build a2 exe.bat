9641e1947397f87496bdfcadd8ce1805c11f73cd
Eric Werner <ewerybody+github@gmail.com>
2018-11-28 18:21:59 +0100

    executable build scripts updated

--- lib/batches/build a2 exe.bat
+++ lib/batches/build a2 exe.bat
@@ -2,8 +2,8 @@ set Ahk2Exe="C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe"
 set this_path=%~dp0
 set lib_path=%this_path%..
 set a2path=%lib_path%\..
-set script=%lib_path%\_source\a2starter.ahk
+set script=%lib_path%\_source\a2_starter.ahk
 set executable=%a2path%\a2.exe
 set icon=%a2path%\ui\res\a2.ico
 
-%Ahk2Exe% /in "%script%" /out "%executable%" /icon %icon% /mpress 1
+%Ahk2Exe% /in "%script%" /out "%executable%" /icon %icon% /mpress 0
