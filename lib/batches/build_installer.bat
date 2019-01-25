fb8773e541e2827ae2c7fd3f9f4a47bc69ca28ff
Eric Werner <ewerybody+github@gmail.com>
2019-01-25 21:09:40 +0100

    added proper version update for the installer manifest
    not minifext 😉

--- lib/batches/build_installer.bat
+++ lib/batches/build_installer.bat
@@ -2,24 +2,44 @@
 
 set a2path=%~dp0..\..
 set iconpath=%a2path%\ui\res\a2.ico
-set Ahk2Exe="C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe"
+set Ahk2Exe=C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe
 set source_path=%a2path%\lib\_source
 set distroot=%a2path%\_ package
 set distpath=%distroot%\a2
 
-set sevenpath=%~dp0\7zr
+set sevenpath=%source_path%\7zr
 set sevenx=%sevenpath%\7zr.exe
-rem set sfx=%sevenpath%\7zSD.sfx
-set sfx=%~dp0\a2.sfx
+set srcsfx=%source_path%\a2_installer.sfx.exe
+set sfx=%distroot%\a2_installer.sfx.exe
 set archive=%distroot%\archive.7z
 set config=%distroot%\config.txt
-set installerx=%distroot%\a2_setup.exe
+set installerx=%distroot%\a2_installer.exe
+set mtx=C:\Program Files (x86)\Windows Kits\10\bin\x86\mt.exe
+set manifest=%distroot%\a2_installer_manifest.xml
+a2_installer_minifext.xml
+echo ### building a2 installer executable ###
+echo distroot: %distroot%
+echo ...
 
-echo building installer script ...
-%Ahk2Exe% /in "%source_path%\setup.ahk" /out "%distpath%\setup.exe" /icon %iconpath% /mpress 0
+REM echo looking for Ahk2Exe: %Ahk2Exe% ...
+REM if not exist "%Ahk2Exe%" (
+  REM echo ERROR: Could not find Ahk2Exe.exe! (at: %Ahk2Exe%)
+  REM pause
+  REM exit
+REM )
 
-echo packing installer archive
-"%sevenx%" a "%archive%" "%distpath%" -m0=BCJ2 -m1=LZMA:d25:fb255 -m2=LZMA:d19 -m3=LZMA:d19 -mb0:1 -mb0s1:2 -mb0s2:3 -mx
+copy "%srcsfx%" "%sfx%" 
+
+if exist "%mtx%" (
+  echo Adding new manifest %manifest% ...
+  "%mtx%" -nologo -manifest "%manifest%" -outputresource:"%sfx%"
+)
+
+echo building installer script executable...
+"%Ahk2Exe%" /in "%source_path%\a2_installer.ahk" /out "%distpath%\setup.exe" /icon %iconpath% /mpress 0
+
+REM echo packing installer archive ...
+REM "%sevenx%" a "%archive%" "%distpath%" -m0=BCJ2 -m1=LZMA:d25:fb255 -m2=LZMA:d19 -m3=LZMA:d19 -mb0:1 -mb0s1:2 -mb0s2:3 -mx
 
 echo building installer executable ...
 copy /b "%sfx%" + "%config%" + "%archive%" "%installerx%"
--- lib/batches/build_py_package.py
+++ lib/batches/build_py_package.py
@@ -6,7 +6,9 @@ Whow! Batch files are such a pain. Better keep them as short as possible.
 import os
 import sys
 import shutil
+import codecs
 from os.path import join
+
 
 PACKAGE_SUB_NAME = 'alpha'
 this_path = os.path.dirname(__file__)
@@ -22,15 +24,17 @@ INSTALLER_CFG = (
     ';!@Install@!UTF-8!\n'
     'RunProgram="a2\setup.exe"\n'
     ';!@InstallEnd@!')
+MANIFEST_NAME = 'a2_installer_manifest.xml'
+
+# adding a2 paths for imports
+a2path = os.path.abspath(join(this_path, '..', '..'))
+uipath = join(a2path, 'ui')
+sys.path.append(uipath)
+import a2ahk
+import a2util
 
 
 def main():
-    a2path = os.path.abspath(join(this_path, '..', '..'))
-    uipath = join(a2path, 'ui')
-    sys.path.append(uipath)
-    import a2ahk
-    import a2util
-
     package_cfg = a2util.json_read(join(a2path, 'package.json'))
     package_name = f'a2 {PACKAGE_SUB_NAME} {package_cfg["version"]}'
     print('package_name: %s' % package_name)
@@ -39,6 +43,9 @@ def main():
     distpath = join(distroot, 'a2')
     if not os.path.isdir(distpath):
         raise FileNotFoundError('No Package found at "%s"!' % distpath)
+
+    srcpath = join(a2path, 'lib', '_source')
+    update_manifest(srcpath, package_cfg["version"], distroot)
 
     print('distpath: %s' % distpath)
     app_path = join(distpath, 'a2app')
@@ -137,6 +144,44 @@ def _ignore_items(path, items):
         print('IGNORING: %s' % result)
 
     return result
+
+
+def update_manifest(srcpath, version, distroot):
+    manifest_path = join(srcpath, MANIFEST_NAME)
+    if not os.path.isfile(manifest_path):
+        print('ERROR: No manifest file: %s' % manifest_path)
+        return
+
+    versions = []
+    for i, n in enumerate(version.split('.')):
+        if not n.isdigit():
+            print('ERROR:\n  Bad Nr in version string %i: %n' % (i, n))
+            continue
+        versions.append(n)
+    versions.extend((4 - len(versions)) * '0')
+
+    content = ''
+    with codecs.open(manifest_path, encoding=a2util.UTF8_CODEC) as fobj:
+        for line in fobj:
+            line = line.strip()
+            if not line:
+                continue
+            if line.startswith('<?xml version="'):
+                continue
+            if line.startswith('<!--'):
+                continue
+
+            if line.startswith('version="'):
+                line = 'version="%s"' % '.'.join(versions)
+                print(line)
+
+            if not line.endswith('>'):
+                line += ' '
+            content += line
+
+    manifest_trg = join(distroot, MANIFEST_NAME)
+    a2util.write_utf8(manifest_trg, content)
+    print('manifest written: %s' % manifest_trg)
 
 
 if __name__ == '__main__':
