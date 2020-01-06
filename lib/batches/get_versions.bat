2a0bef13dde84b3bd023e4854312a142fb8e5b94
Eric Werner <ewerybody+github@gmail.com>
2020-01-06 19:12:02 +0100

    fixed file names in get_versions-test-bat

--- lib/batches/get_versions.bat
+++ lib/batches/get_versions.bat
@@ -5,19 +5,19 @@ set ahk_exe=%here%..\Autohotkey\Autohotkey.exe
 set tmp_txt=_ sfdgsdfgsdfgsdfg.txt
 
 rem get the version of our Autohotkey.exe
-"%ahk_exe%" versions\get_ahk_version.ahk > "%tmp_txt%"
+"%ahk_exe%" versions\get_AutoHotkey_version.ahk > "%tmp_txt%"
 set /p ahk_version= < "%tmp_txt%"
 del "%tmp_txt%"
 
 echo ahk_version: %ahk_version%
 
-"%ahk_exe%" versions\get_python_version.ahk > "%tmp_txt%"
+"%ahk_exe%" versions\get_Python_version.ahk > "%tmp_txt%"
 set /p python= < "%tmp_txt%"
 del "%tmp_txt%"
 
 echo python: %python%
 
-"%ahk_exe%" versions\get_pyside_version.ahk > "%tmp_txt%"
+"%ahk_exe%" versions\get_PySide2_version.ahk > "%tmp_txt%"
 set /p pyside_version= < "%tmp_txt%"
 del "%tmp_txt%"
 
