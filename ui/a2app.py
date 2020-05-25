38ebcfa9c9e0f07326abaf548e22ea88a130c2ba
Eric Werner <ewerybody+github@gmail.com>
2020-05-25 14:28:07 +0200

    Moved main a2 app to subclass QSingleApp
    All a little nicer this way. Removed the need for these globals.

--- ui/a2app.py
+++ ui/a2app.py
@@ -15,84 +15,101 @@ from PySide2 import QtWidgets
 from singlesiding import QSingleApplication
 
 logging.basicConfig()
-log = logging.getLogger('a2app')
-log.setLevel(logging.DEBUG)
 
 
-app = None
-a2win = None
+class A2Main(QSingleApplication):
+    """The a2 foundation object."""
+    def __init__(self):
+        super(A2Main, self).__init__(sys.argv)
+        # self._app = None
+        self._core = None
+        self._win = None
+        self._log = logging.getLogger('a2app')
+        self._log.setLevel(logging.DEBUG)
+
+        # ensure_single will already exit() after sending the message
+        if not sys.argv[1:]:
+            self.ensure_single(['--show'])
+        else:
+            self.ensure_single()
+
+        self.message_received.connect(self.app_msg_get)
+        self.lastWindowClosed.connect(self.last_window_closed)
+
+        # adding PySide plugin paths. e.g. to make all the imageformats available
+        pyside_plugin_path = os.path.join(sys.modules['PySide2'].__path__[0], 'plugins')
+        self.addLibraryPath(pyside_plugin_path)
+
+        winfo = platform.uname()
+        self.info('initialised!\n  python: %s\n  windows: %s', sys.version, str(winfo)[31:-1])
+
+        # this is to set the actual taskbar icon
+        windll.shell32.SetCurrentProcessExplicitAppUserModelID('ewerybody.a2.0.1')
+
+        self.init_a2_win()
+
+    def last_window_closed(self):
+        """Handle last window deletion."""
+        self.info('a2 lastWindowClosed!')
+        self.exit()
+
+    def init_a2_win(self):
+        """Load main a2 submodules and instantiate them."""
+        this_dir = os.path.abspath(os.path.dirname(__file__))
+        if os.getcwd() != this_dir:
+            self.info('Bending cwd to "%s"', this_dir)
+            os.chdir(this_dir)
+
+        try:
+            import a2core
+            self._core = a2core.A2Obj.inst()
+            self._core.start_up()
+
+            import a2ui
+            self._win = a2ui.A2Window(self)
+            self._core.win = self._win
+            self._core.app = self
+            self._core.win.showRaise()
+
+        except Exception as error:
+            # TODO: provide more detailed startup error report
+            # error_class, error_msg, trace_back = sys.exc_info()
+            title = 'a2app: Error on "init_a2_win()"!'
+            msg = ('Could not call A2Window! Error:\n%s\n'
+                   'Traceback:%s\n\nPress Ctrl+C to copy this message.'
+                   % (error, traceback.format_exc().strip()))
+            QtWidgets.QMessageBox.critical(None, title, msg)
+            raise RuntimeError(msg)
+
+    def app_msg_get(self, msg):
+        """
+        Handle received messeges with.
+        """
+        if '--close' in msg:
+            self.exit()
+
+        elif '--show' in msg:
+            self.info('received show command ...')
+            self._win.showRaise()
+
+        elif '--reload' in msg:
+            self.error('reload is deprecated!')
+
+        else:
+            self.error('received unhandled message: %s', ' '.join(msg))
+
+    def info(self, *msg):
+        """Log an info message"""
+        self._log.info(*msg)
+
+    def error(self, *msg):
+        """Log an error message"""
+        self._log.error(*msg)
 
 
 def main():
-    global app, a2win
-    app = QSingleApplication(sys.argv)
-
-    # ensure_single will already exit() after sending the message
-    if not sys.argv[1:]:
-        app.ensure_single(['--show'])
-    else:
-        app.ensure_single()
-    app.message_received.connect(app_msg_get)
-    app.lastWindowClosed.connect(last_window_closed)
-
-    # adding PySide plugin paths. e.g. to make all the imageformats available
-    pyside_plugin_path = os.path.join(sys.modules['PySide2'].__path__[0], 'plugins')
-    app.addLibraryPath(pyside_plugin_path)
-
-    winfo = platform.uname()
-    log.info('initialised!\n  python: %s\n  windows: %s', sys.version, str(winfo)[31:-1])
-
-    # this is to set the actual taskbar icon
-    windll.shell32.SetCurrentProcessExplicitAppUserModelID('ewerybody.a2.0.1')
-
-    a2win = init_a2_win(app)
+    app = A2Main()
     app.exec_()
-
-
-def last_window_closed():
-    log.info('a2 lastWindowClosed!')
-    app.exit()
-
-
-def init_a2_win(app):
-    this_dir = os.path.abspath(os.path.dirname(__file__))
-    if os.getcwd() != this_dir:
-        log.info('Bending cwd to "%s"', this_dir)
-        os.chdir(this_dir)
-
-    try:
-        import a2core
-        a2 = a2core.A2Obj.inst()
-        a2.start_up()
-
-        import a2ui
-        a2.win = a2ui.A2Window(app)
-        a2.app = app
-        a2.win.showRaise()
-
-    except Exception as error:
-        # TODO: provide more detailed startup error report
-        # error_class, error_msg, trace_back = sys.exc_info()
-        title = 'a2app: Error on "init_a2_win()"!'
-        msg = ('Could not call A2Window! Error:\n%s\n'
-               'Traceback:%s\n\nPress Ctrl+C to copy this message.'
-               % (error, traceback.format_exc().strip()))
-        QtWidgets.QMessageBox.critical(None, title, msg)
-        raise RuntimeError(msg)
-    return a2.win
-
-
-def app_msg_get(msg):
-    # global app, a2win
-    if '--close' in msg:
-        app.exit()
-    elif '--show' in msg:
-        log.info('received show command ...')
-        a2win.showRaise()
-    elif '--reload' in msg:
-        log.error('reload is deprecated!')
-    else:
-        log.info('received unhandled message: %s',  ' '.join(msg))
 
 
 if __name__ == '__main__':
