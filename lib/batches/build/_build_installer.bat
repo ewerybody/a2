0171b44a423bfd1d2cec44aaf59bd44d25f185dd
Eric Werner <ewerybody+github@gmail.com>
2020-07-15 15:21:00 +0200

    added building uninstaller #187

--- lib/batches/build/_build_installer.bat
+++ lib/batches/build/_build_installer.bat
@@ -2,15 +2,7 @@ rem This actually ONLY calls the Python installer builder!
 @echo off
 set here=%~dp0
 set a2path=%here%..\..\..
-set iconpath=%a2path%\ui\res\a2.ico
-set source_path=%a2path%\lib\_source
 set distroot=%a2path%\_ package
-set distpath=%distroot%\a2
-set sfx=%distroot%\a2_installer.sfx.exe
-set archive=%distroot%\archive.7z
-set config=%distroot%\config.txt
-set installerx=%distroot%\a2_installer.exe
-
 
 rem getting latest python path
 set ahk_exe=%here%..\..\Autohotkey\Autohotkey.exe
--- lib/batches/build/_finish_py_package.bat
+++ lib/batches/build/_finish_py_package.bat
@@ -5,7 +5,9 @@ set source_path=%a2path%\lib\_source
 set distroot=%a2path%\_ package
 set distpath=%distroot%\a2
 set Ahk2Exe="C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe"
-set iconpath=%a2path%\ui\res\a2.ico
+set iconpaths=%a2path%\ui\res\
+set a2icon=%iconpaths%a2.ico
+set a2xicon=%iconpaths%a2x.ico
 
 rem getting latest python path
 set ahk_exe=%here%..\..\Autohotkey\Autohotkey.exe
@@ -19,9 +21,12 @@ echo running py build script ...
 %pypath%\python.exe %here%finish_package.py
 
 echo building root a2ui executable ...
-%Ahk2Exe% /in "%source_path%\a2ui_starter.ahk" /out "%distpath%\a2ui.exe" /icon %iconpath% /mpress 0
+%Ahk2Exe% /in "%source_path%\a2ui_starter.ahk" /out "%distpath%\a2ui.exe" /icon "%a2icon%" /mpress 0
 
 echo building root a2 executable ...
-%Ahk2Exe% /in "%source_path%\a2_starter.ahk" /out "%distpath%\a2.exe" /icon %iconpath% /mpress 0
+%Ahk2Exe% /in "%source_path%\a2_starter.ahk" /out "%distpath%\a2.exe" /icon "%a2icon%" /mpress 0
+
+echo building root a2 Uninstaller executable ...
+%Ahk2Exe% /in "%source_path%\a2_uninstaller.ahk" /out "%distpath%\Uninstall a2.exe" /icon "%a2xicon%" /mpress 0
 
 echo ######## Finish Py Package Done! ########
