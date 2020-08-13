9e851bcd6e1442a6243b0ffee95eed5c1bc363af
Eric Werner <ewerybody+github@gmail.com>
2020-08-13 22:11:36 +0200

    added a little iconsparser to keep the code uptodate

--- ui/a2ctrl/icons.py
+++ ui/a2ctrl/icons.py
@@ -1,4 +1,5 @@
 import os
+import time
 import a2core
 import a2path
 
@@ -12,9 +13,12 @@ LOW_ALPHA = 0.25
 DEFAULT_NAME = f'{a2core.NAME}icon'
 ICON_FORMATS = ['.svg', '.png', '.ico']
 ICON_TYPES = [DEFAULT_NAME + ext for ext in ICON_FORMATS]
+ICON_OBJ_INST_ERROR = 'Icons() has already been instanciated!\nGet it with .inst()'
 
 
 class Ico(QtGui.QIcon):
+    ico_path = None
+
     def __init__(self, ico_name, px=512, alpha=None):
         """
         :param str ico_name: Name of the icon. If present in icon library the path is
@@ -33,16 +37,16 @@ class Ico(QtGui.QIcon):
         self._painter = None
         self._image = None
 
-        if os.path.exists(ico_name):
+        if os.path.isfile(ico_name):
             self.path = ico_name
         else:
-            global ICO_PATH
-            if ICO_PATH is None:
-                ICO_PATH = os.path.join(a2core.A2Obj.inst().paths.a2, 'ui', 'res', '%s.svg')
+            if Ico.ico_path is None:
+                Ico.ico_path = os.path.join(a2core.A2Obj.inst().paths.a2, 'ui', 'res', '%s.svg')
+                log.info('getting Ico.ico_path: %s', Ico.ico_path)
 
-            self.path = ICO_PATH % ico_name
-            if not os.path.exists(self.path):
-                log.error('SVG_icon: could not find path to "%s"!' % ico_name)
+            self.path = Ico.ico_path % ico_name
+            if not os.path.isfile(self.path):
+                log.error('SVG_icon: could not find path to "%s"!', ico_name)
                 return
 
         ext = os.path.splitext(self.path)[1].lower()
@@ -89,18 +93,22 @@ class Ico(QtGui.QIcon):
 
     @property
     def tinted(self):
+        """Pass a tinted version of the same icon."""
         if self._tinted is None:
             self._tinted = Ico(self.path, self.px, alpha=LOW_ALPHA)
-
         return self._tinted
 
 
 class LibIco(Ico):
+    """Ico variant with hardcoded specs."""
     def __init__(self, name):
         super(LibIco, self).__init__(name, 512, DEFAULT_ALPHA)
 
 
 class Icons(object):
+    # This is supposed to have many!
+    # pylint: disable=too-many-instance-attributes
+    """Our load-only-once icon library object."""
     _instance = None
 
     @staticmethod
@@ -112,26 +120,40 @@ class Icons(object):
             Icons._instance = Icons()
         return Icons._instance
 
+    def __getattribute__(self, name):
+        try:
+            obj = super(Icons, self).__getattribute__(name)
+        except AttributeError:
+            obj, name
+        if not name.startswith('_'):
+            if isinstance(obj, bool):
+                obj, name
+            return obj
+        else:
+            return obj
+
     def __init__(self):
         if self._instance:
-            raise RuntimeError('Icons() has already been instanciated!\n'
-                               'get it with .inst()')
+            raise RuntimeError(ICON_OBJ_INST_ERROR)
+        t0 = time.time()
 
-        self.a2 = Ico(a2core.NAME)
-        self.a2close = Ico('a2x')
-        self.a2reload = Ico('a2reload')
+        # Icons start
+        self.a2 = Ico('a2')
         self.a2help = Ico('a2help')
+        self.a2reload = Ico('a2reload')
+        self.a2tinted = Ico('a2tinted')
+        self.a2x = Ico('a2x')
         self.autohotkey = Ico('autohotkey')
         self.github = Ico('github')
 
         self.button = LibIco('button')
         self.check = LibIco('check')
         self.check_circle = LibIco('check_circle')
-        self.code = LibIco('code')
-        self.copy = LibIco('copy')
-        self.combo = LibIco('combo')
         self.clear = LibIco('clear')
         self.cloud_download = LibIco('cloud_download')
+        self.code = LibIco('code')
+        self.combo = LibIco('combo')
+        self.copy = LibIco('copy')
         self.cut = LibIco('cut')
         self.delete = LibIco('delete')
         self.down = LibIco('down')
@@ -145,7 +167,7 @@ class Icons(object):
         self.folder_add = LibIco('folder_add')
         self.group = LibIco('group')
         self.help = LibIco('help')
-        self.hotkey = LibIco('keyboard')
+        self.keyboard = LibIco('keyboard')
         self.label = LibIco('label')
         self.label_plus = LibIco('label_plus')
         self.list_add = LibIco('list_add')
@@ -153,18 +175,21 @@ class Icons(object):
         self.more = LibIco('more')
         self.number = LibIco('number')
         self.paste = LibIco('paste')
-        self.text = LibIco('text')
         self.reload = LibIco('reload')
         self.rollback = LibIco('rollback')
-        self.scope_global = LibIco('global')
         self.scope = LibIco('scope')
         self.scope_exclude = LibIco('scope_exclude')
+        self.scope_global = LibIco('scope_global')
         self.string = LibIco('string')
+        self.text = LibIco('text')
         self.up = LibIco('up')
         self.up_align = LibIco('up_align')
+        # Icons end
+        print('%s took %.3fs' % ('Icons', time.time() - t0))
 
 
 def get(current_icon, folder, fallback=None):
+    """Find an icon path or fallback."""
     if current_icon is None or not os.path.isfile(current_icon.path):
         icon_path = ''
         for item in a2path.iter_files(folder):
@@ -180,3 +205,59 @@ def get(current_icon, folder, fallback=None):
             current_icon = fallback
 
     return current_icon
+
+
+def _update_icon_stub():
+    from fnmatch import fnmatch
+    with open(__file__) as file_obj:
+        content = file_obj.read()
+
+    lines = []
+    full_color_names = ('a2*', 'autohotkey', 'github')
+    ignore_these = ('telegram_join', 'css_*', 'logo_*')
+    full_color = set()
+    lib_icons = set()
+    in_icons = False
+
+    for line in content.split('\n'):
+        if line.endswith('# Icons start'):
+            lines.append(line)
+
+            in_icons = True
+            res_path = os.path.abspath(os.path.join(__file__, '..', '..', 'res'))
+            for icon_item in a2path.iter_types(res_path, ICON_FORMATS):
+                if any(fnmatch(icon_item.base, name) for name in ignore_these):
+                    continue
+
+                if any(fnmatch(icon_item.base, name) for name in full_color_names):
+                    full_color.add(icon_item.base)
+                else:
+                    lib_icons.add(icon_item.base)
+
+            indent = ' ' * 8
+            for name in sorted(full_color):
+                lines.append(f"{indent}self.{name} = Ico('{name}')")
+            lines.append('')
+            for name in sorted(lib_icons):
+                lines.append(f"{indent}self.{name} = LibIco('{name}')")
+
+        if line.endswith('# Icons end'):
+            in_icons = False
+        if in_icons:
+            continue
+        lines.append(line)
+
+    new_content = '\n'.join(lines)
+    num_icons = len(full_color) + len(lib_icons)
+    if new_content != content:
+        new_name = __file__ + ' _ changed'
+        with open(new_name, 'w') as file_obj:
+            file_obj.write(new_content)
+        print(f'Total of {num_icons} icons written into the code.'
+              f'look into {new_name} to see the changes!')
+    else:
+        print(f'Nothing changed! All {num_icons} icons already listed!')
+
+
+if __name__ == "__main__":
+    _update_icon_stub()
