92dbc5c47073676076ac8b61a7f760ddc6a91252
Eric Werner <ewerybody+github@gmail.com>
2021-01-08 16:04:18 +0100

    Qt for Python wrapper prefers PySide2 for now
    all re-generated

--- ui/a2qt/QtNetwork.py
+++ ui/a2qt/QtNetwork.py
@@ -2,6 +2,7 @@
 a2qt Qt for Python wrapper.
 """
 import a2qt
+
 if a2qt.QT_VERSION == 6:
     from PySide6.QtNetwork import *
 
--- ui/a2qt/__init__.py
+++ ui/a2qt/__init__.py
@@ -1,12 +1,15 @@
 """
 Wrapper package to work with both PySide2 and PySide6.
-Whatever is installed. Prefering the latest.
+
+Whatever is installed. Prefering stable PySide2 for now. Soon to be latest!
 """
 try:
-    import PySide6
-    QT_VERSION = 6
-    QT_PATH = PySide6.__path__[0]
-except ImportError:
     import PySide2
+
     QT_VERSION = 5
     QT_PATH = PySide2.__path__[0]
+except ImportError:
+    import PySide6
+
+    QT_VERSION = 6
+    QT_PATH = PySide6.__path__[0]
