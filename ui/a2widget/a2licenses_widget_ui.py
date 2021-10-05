ddfd0a0130ff6989c786c0e504b8d76a19434bfd
Eric Werner <ewerybody+github@gmail.com>
2021-10-06 00:52:40 +0200

    settings page material icons links updated

--- ui/a2widget/a2licenses_widget_ui.py
+++ ui/a2widget/a2licenses_widget_ui.py
@@ -13,7 +13,7 @@ from a2qt.QtGui import *
 from a2qt.QtWidgets import *
 
 
-class Ui_Form(object):
+class Ui_Form:
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
@@ -25,15 +25,15 @@ class Ui_Form(object):
         self.scrollArea.setWidgetResizable(True)
         self.scrollAreaWidgetContents = QWidget()
         self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 631, 627))
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 800, 755))
         self.verticalLayout_2 = QVBoxLayout(self.scrollAreaWidgetContents)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
         self.a2license_text = QLabel(self.scrollAreaWidgetContents)
         self.a2license_text.setObjectName(u"a2license_text")
         self.a2license_text.setText(u"<html><head/><body><p align=\"center\"><br/></p><p align=\"center\"><img src=\"res/logo_ahk.png\"/><br/><span style=\" font-weight:600;\">AutoHotkey</span> - a scripting language for desktop automation - {ahk_version}</p><p align=\"center\"><a href=\"https://autohotkey.com\"><span style=\" text-decoration: underline; color:#0000ff;\">autohotkey.com</span></a> - <a href=\"https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html\"><span style=\" text-decoration: underline; color:#0000ff;\">GNU GPLv2</span></a></p><p align=\"center\"><br/></p><p align=\"center\"><img src=\"res/logo_python.png\"/></p><p align=\"center\"><span style=\" font-weight:600;\">Python</span> - high-level programming language - {py_version}</p><p align=\"center\"><a href=\"https://www.python.org/\"><span style=\" text-decoration: underline; color:#0000ff;\">python.org</span></a> - <a href=\"https://docs.python.org/3/license.html\"><span style=\" text-decoration: underline; color:#0000ff;\">Python Software Foundation License - GPL compatib"
-                        "le</span></a></p><p align=\"center\"><br/></p><p align=\"center\"><img src=\"res/logo_pyside.png\"/></p><p align=\"center\"><span style=\" font-weight:600;\">Qt for Python</span> - Python binding of the cross-platform GUI toolkit Qt - {qt_version}</p><p align=\"center\"><a href=\"https://www.qt.io/qt-for-python\"><span style=\" text-decoration: underline; color:#0000ff;\">qt.io/qt-for-python</span></a> - <a href=\"https://www.qt.io/licensing\"><span style=\" text-decoration: underline; color:#0000ff;\">LGPLv3/GPLv2</span></a></p><p align=\"center\"><br/></p><p align=\"center\"><img src=\"res/logo_material.png\"/></p><p align=\"center\"><span style=\" font-weight:600;\">Material Icons</span> - Material Design icons by Google</p><p align=\"center\"><a href=\"https://material.io/icons\"><span style=\" text-decoration: underline; color:#0000ff;\">material.io/icons</span></a> - <a href=\"https://www.apache.org/licenses/LICENSE-2.0.html\"><span style=\" text-decoration: underline; color:#0000ff;\">Apache License Ver"
-                        "sion 2.0</span></a></p><p align=\"center\"><br/></p><p align=\"center\">Thanks so much!</p><p align=\"center\"><br/></p></body></html>")
+                        "le</span></a></p><p align=\"center\"><br/></p><p align=\"center\"><img src=\"res/logo_pyside.png\"/></p><p align=\"center\"><span style=\" font-weight:600;\">Qt for Python</span> - Python binding of the cross-platform GUI toolkit Qt - {qt_version}</p><p align=\"center\"><a href=\"https://www.qt.io/qt-for-python\"><span style=\" text-decoration: underline; color:#0000ff;\">qt.io/qt-for-python</span></a> - <a href=\"https://www.qt.io/licensing\"><span style=\" text-decoration: underline; color:#0000ff;\">LGPLv3/GPLv2</span></a></p><p align=\"center\"><br/></p><p align=\"center\"><img src=\"res/logo_material.png\"/></p><p align=\"center\"><span style=\" font-weight:600;\">Material Icons</span> - Material Design icons by Google</p><p align=\"center\"><a href=\"https://fonts.google.com/icons\"><span style=\" text-decoration: underline; color:#0000ff;\">fonts.google.com/icons</span></a> - <a href=\"https://www.apache.org/licenses/LICENSE-2.0.html\"><span style=\" text-decoration: underline; color:#0000ff;\">Apache L"
+                        "icense Version 2.0</span></a></p><p align=\"center\"><br/></p><p align=\"center\">Thanks so much!</p><p align=\"center\"><br/></p></body></html>")
         self.a2license_text.setWordWrap(True)
         self.a2license_text.setOpenExternalLinks(True)
         self.verticalLayout_2.addWidget(self.a2license_text)
