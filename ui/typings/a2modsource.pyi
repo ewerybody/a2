01bf5a11790fee6e1784c05c97eb0464a086fa12
Eric Werner <ewerybody+github@gmail.com>
2021-11-03 21:18:15 +0100

    Create a2modsource.pyi

---
+++ ui/typings/a2modsource.pyi
@@ -0,0 +1,9 @@
+import a2core
+import a2mod
+from a2qt import QtGui
+
+class ModSource:
+    name: str
+    icon: QtGui.QIcon
+    mods: dict[str, a2mod.Mod]
+    def __init__(self, a2: a2core.A2Obj, name: str) -> None: ...
