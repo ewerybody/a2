1c23d76d75f71934979b6025722e3675992cd4fc
Eric Werner <ewerybody+github@gmail.com>
2022-01-31 23:41:49 +0100

    Create test_window.py

---
+++ ui/a2widget/demo/test_window.py
@@ -0,0 +1,20 @@
+from a2qt import QtCore, QtWidgets
+import a2ctrl
+
+class TestWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        widget = QtWidgets.QWidget()
+        self.setCentralWidget(widget)
+        self.setWindowIcon(a2ctrl.Icons.a2)
+
+
+def show():
+    app = QtWidgets.QApplication([])
+    win = TestWindow()
+    win.show()
+    app.exec()
+
+
+if __name__ == '__main__':
+    show()
