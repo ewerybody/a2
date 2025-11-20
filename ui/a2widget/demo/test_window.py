d795487212406e7dc91d9d65efbaafa38d21ea8a
Eric Werner <ewerybody+github@gmail.com>
2025-11-20 16:11:44 +0100

    Update test_window.py

--- ui/a2widget/demo/test_window.py
+++ ui/a2widget/demo/test_window.py
@@ -1,5 +1,8 @@
-from a2qt import QtCore, QtWidgets
+import sys
+
+from PySide6 import QtCore, QtWidgets, QtGui
 import a2ctrl
+
 
 class TestWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -7,6 +10,12 @@ class TestWindow(QtWidgets.QMainWindow):
         widget = QtWidgets.QWidget()
         self.setCentralWidget(widget)
         self.setWindowIcon(a2ctrl.Icons.a2)
+
+        layout = QtWidgets.QFormLayout(widget)
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        layout.addRow('Python:', QtWidgets.QLabel(sys.version))
+        layout.addRow('Qt:', QtWidgets.QLabel(QtCore.qVersion()))
 
 
 def show():
