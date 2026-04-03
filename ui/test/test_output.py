175c65a81aca3801e7d1bf28a40cf3f4b8e352bd
Eric Werner <ewerybody+github@gmail.com>
2026-04-03 16:52:18 +0200

    Update test_output.py

--- ui/test/test_output.py
+++ ui/test/test_output.py
@@ -1,11 +1,19 @@
+import pytest
+
 import a2output
 
 
 def test_get_set_vars():
     bucket = []
+
     def test(msg, bucket=bucket):
         bucket.append(msg)
+
     a2output.connect(test)
     assert bucket == []
     print('something')
     assert bucket != []
+
+
+if __name__ == '__main__':
+    pytest.main([__file__, '-v'])
