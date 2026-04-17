064a6ca62aa8fd806503e487412a34cb11e6d530
Eric Werner <ewerybody+github@gmail.com>
2026-04-17 13:46:37 +0200

    7zip dep script: new `add_to_archive`

--- ui/a2dev/dependency/seven_zip.py
+++ ui/a2dev/dependency/seven_zip.py
@@ -9,6 +9,7 @@ import a2path
 
 from a2dev.dependency import get_github_latest_release
 
+# Perform lazy import to avoid cyclic import problems
 # from a2dev.build import Paths, CHK_MK, EX_MRK, DownloadCB
 from a2util import version_tuplify
 
@@ -22,26 +23,24 @@ SFX_CLI = '7zS2con.sfx'
 SDK_NAME = 'lzma'
 GITHUB_OWNER = 'ip7z'
 FILES = (SFX_UI, SFX_CLI, 'lzma-sdk.txt')
-
+EVERYTHING_OK = 'Everything is Ok'
 
 def check():
     """Make sure 7-Zip tools needed for building the self extracting installer are available
     and up-to-date.
     """
-    from a2dev.build import Paths, CHK_MK, EX_MRK, DownloadCB
+    from a2dev.build import CHK_MK, EX_MRK, DownloadCB
 
     print(f'  Looking up latest {DISPLAY} online ...')
     release = get_github_latest_release(GITHUB_OWNER, DISPLAY)
     latest_version = version_tuplify(release.get('tag_name', ''))
-    seven_zip_dir = os.path.join(Paths.source, NAME)
-    seven_zip_exe = os.path.join(seven_zip_dir, EXE)
+    seven_zip_dir, seven_zip_exe = get_paths()
     if os.path.isfile(seven_zip_exe):
         current_version = version_tuplify(a2ahk.call_lib_cmd('get_version', seven_zip_exe))
         if current_version >= latest_version:
             print(f'{CHK_MK} {EXE} already up-to-date {current_version}!')
             return
         shutil
-
 
     print(f'  Fetching {DISPLAY} {latest_version} ...')
     asset = next((a for a in release['assets'] if a['name'] == EXE), None)
@@ -67,6 +66,31 @@ def check():
             os.rename(item.path, os.path.join(seven_zip_dir, item.name))
 
     print(f'  {CHK_MK} {DISPLAY} updated {latest_version}!')
+
+
+def add_to_archive(source, destination_archive, indent=''):
+    from a2dev.build import CHK_MK
+    _, exe_path = get_paths()
+    print(f'{indent}Adding to archive ...')
+    args = [exe_path, 'a', destination_archive, source] + FLAGS
+    process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    assert process.stdout is not None
+
+    line_nr = 0
+    for line in process.stdout:
+        print(f'{indent}  7zip {line_nr}: {line}', end='')
+        line_nr += 1
+    process.wait()
+    if line.strip() == EVERYTHING_OK:
+        print(f'{indent}{CHK_MK} {EVERYTHING_OK}!')
+
+
+def get_paths():
+    from a2dev.build import Paths
+
+    dir_path = os.path.join(Paths.source, NAME)
+    exe_path = os.path.join(dir_path, EXE)
+    return dir_path, exe_path
 
 
 if __name__ == '__main__':
