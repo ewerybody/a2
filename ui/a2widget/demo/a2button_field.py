46191c0f8ffad1c97bffd535228d7af78d82dae8
Eric Werner <ewerybody+github@gmail.com>
2026-04-19 22:59:45 +0200

    Update a2button_field.py

--- ui/a2widget/demo/a2button_field.py
+++ ui/a2widget/demo/a2button_field.py
@@ -34,7 +34,6 @@ class Demo(QtWidgets.QMainWindow):
 
 
 def show():
-    # app = QtWidgets.QApplication([__file__, '-platform', 'windows:darkmode=2'])
     app = QtWidgets.QApplication([])
     win = Demo()
     win.show()
